bd83c3a2207d6b1eef6dbca31a5ad068fbe6f8eb
eugeniosant <eugenio.santoboni@gmail.com>
2014-09-18 09:27:10 +0200

    Rename bundle folders

--- ui-bundles/entando-bundles-contents/entando-content-download/src/main/resources/sql/bundles/entando-content-download/port_data_production.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_CATEGORIES', 'en', 'Tags');
-INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_CATEGORIES', 'it', 'Tag');
-
-INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_GET_INFO', 'en', 'View details');
-INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_GET_INFO', 'it', 'Dettagli');
-
-INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_DOWNLOAD', 'en', 'Get it now');
-INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_DOWNLOAD', 'it', 'Scarica');
