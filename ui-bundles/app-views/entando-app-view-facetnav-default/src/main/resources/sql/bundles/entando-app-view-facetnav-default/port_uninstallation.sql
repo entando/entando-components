7217b757dcbbf0dcb2fd5f55a7d93ea977b5d951
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2014-12-15 18:01:47 +0100

    jpfacetnav: upgrade uninstallation scripts

--- ui-bundles/app-views/entando-app-view-facetnav-default/src/main/resources/sql/bundles/entando-app-view-facetnav-default/port_uninstallation.sql
+++ ui-bundles/app-views/entando-app-view-facetnav-default/src/main/resources/sql/bundles/entando-app-view-facetnav-default/port_uninstallation.sql
@@ -1,6 +1,10 @@
 DELETE FROM widgetconfig WHERE widgetcode = 'jpfacetnav_results';
 DELETE FROM widgetconfig WHERE widgetcode = 'jpfacetnav_tree';
+DELETE FROM widgetconfig WHERE widgetcode IN (SELECT code FROM widgetcatalog WHERE parenttypecode = 'jpfacetnav_results');
+DELETE FROM widgetconfig WHERE widgetcode IN (SELECT code FROM widgetcatalog WHERE parenttypecode = 'jpfacetnav_tree');
 
+DELETE FROM widgetcatalog WHERE parenttypecode = 'jpfacetnav_results';
+DELETE FROM widgetcatalog WHERE parenttypecode = 'jpfacetnav_tree';
 DELETE FROM widgetcatalog WHERE code = 'jpfacetnav_results';
 DELETE FROM widgetcatalog WHERE code = 'jpfacetnav_tree';
 
