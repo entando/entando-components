1e60767518fef2d23ef1a03a12785eaea1a2a375
eugeniosant <eugenio.santoboni@gmail.com>
2015-09-25 15:39:43 +0200

    upgrade Copyright

--- plugins/entando-plugin-jpsurvey/src/test/java/com/agiletec/plugins/jpsurvey/apsadmin/survey/TestChoiceAction.java
+++ plugins/entando-plugin-jpsurvey/src/test/java/com/agiletec/plugins/jpsurvey/apsadmin/survey/TestChoiceAction.java
@@ -21,23 +21,6 @@
  */
 package com.agiletec.plugins.jpsurvey.apsadmin.survey;
 
-/*
-*
-* Copyright 2013 Entando S.r.l. (http://www.entando.com) All rights reserved.
-*
-* This file is part of Entando software. 
-* Entando is a free software; 
-* You can redistribute it and/or modify it
-* under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; version 2.
-* 
-* See the file License for the specific language governing permissions   
-* and limitations under the License
-* 
-* 
-* 
-* Copyright 2013 Entando S.r.l. (http://www.entando.com) All rights reserved.
-*
-*/
 import com.agiletec.plugins.jpsurvey.apsadmin.ApsAdminPluginBaseTestCase;
 
 import com.agiletec.apsadmin.system.ApsAdminSystemConstants;
@@ -46,14 +29,14 @@ import com.agiletec.plugins.jpsurvey.aps.system.services.SurveySystemConstants;
 import com.agiletec.plugins.jpsurvey.aps.system.services.collect.IResponseManager;
 import com.agiletec.plugins.jpsurvey.aps.system.services.survey.ISurveyManager;
 import com.agiletec.plugins.jpsurvey.aps.system.services.survey.model.Survey;
-import com.agiletec.plugins.jpsurvey.apsadmin.survey.ChoiceAction;
 
 public class TestChoiceAction extends ApsAdminPluginBaseTestCase {
-
+	
+	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		this.init();
-		}
+	}
 
 	private void init() {
 		this.setSurveyManager((ISurveyManager) this.getService(SurveySystemConstants.SURVEY_MANAGER));
