5906f8660cd1ffa61b5673e9442032af452b9893
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2017-03-06 13:37:05 +0100

    login widget bundle: add missing label

--- ui-bundles/widgets/entando-widget-login_form/src/main/resources/sql/bundles/entando-widget-login_form/port_data_production.sql
+++ ui-bundles/widgets/entando-widget-login_form/src/main/resources/sql/bundles/entando-widget-login_form/port_data_production.sql
@@ -72,24 +72,19 @@ INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, lock
 
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_WELCOME', 'en', 'Hello');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_WELCOME', 'it', 'Ciao');
-
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_ADMINISTRATION', 'en', 'Administration');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_ADMINISTRATION', 'it', 'Amministrazione');
-
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_SIGNOUT', 'en', 'Sign out');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_SIGNOUT', 'it', 'Esci');
-
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_SIGNIN', 'en', 'Sign in');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_SIGNIN', 'it', 'Accedi');
-
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_USER_STATUS_EXPIRED', 'en', 'Your account has expired!');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_USER_STATUS_EXPIRED', 'it', 'Credenziali scadute!');
-
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_USER_STATUS_CREDENTIALS_INVALID', 'en', 'Wrong username or password!');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_USER_STATUS_CREDENTIALS_INVALID', 'it', 'Le credenziali non sono corrette!');
-
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_USERNAME', 'en', 'Username');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_USERNAME', 'it', 'Utente');
-
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_PASSWORD', 'en', 'Password');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_PASSWORD', 'it', 'Password');
+INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_PROFILE_CONFIGURATION', 'en', 'Edit profile');
+INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_PROFILE_CONFIGURATION', 'it', 'Modifica profilo');
