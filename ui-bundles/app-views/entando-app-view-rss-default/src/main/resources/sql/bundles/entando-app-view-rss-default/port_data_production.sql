e26536caa09c2f50256b5bd5489b9cc48fcf3ea5
Davide <d.minnai@entando.com>
2017-06-14 12:50:10 +0200

    modified port_data.sql for pprss_channels.jsp fragment

--- ui-bundles/app-views/entando-app-view-rss-default/src/main/resources/sql/bundles/entando-app-view-rss-default/port_data_production.sql
+++ ui-bundles/app-views/entando-app-view-rss-default/src/main/resources/sql/bundles/entando-app-view-rss-default/port_data_production.sql
@@ -10,18 +10,20 @@ INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode,
 INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('jprss_rssChannels', 'jprss_rssChannels', 'jprss', NULL, '<#assign wp=JspTaglibs["/aps-core"]>
 <#assign jprss=JspTaglibs["/jprss-core"]>
 
+<link rel="stylesheet" href="<@wp.resourceURL />plugins/jprss/administration/common/css/jprss-administration.css">
+
 <@jprss.rssList listName="rssList" />
 <@wp.info key="langs" var="langs" />
 
-<h1><@wp.i18n key="jprss_FEED_LIST"/></h1>
+<p class="rssChannelTitle"><@wp.i18n key="jprss_FEED_LIST"/></p>
 <dl class="dl-horizontal">
 <#if (rssList??) && (rssList?has_content) && (rssList?size > 0)>
 	<#list rssList as channel>
-		<h2><span class="icon  icon-th-list"></span>&#32;${channel.title}</h2>
-		<p>${channel.description}</p> 
+		<p class="rssTitle"><span class="icon  icon-th-list"></span>&#32;${channel.title}</p>
+		<p class="rssChannelDescr">${channel.description}</p> 
 		<ul class="unstyled inline margin-medium-bottom">
 			<#list langs as channelLang>
-				<li class="inline">
+				 <li class="inline rss">
 					<a href="<@wp.info key="systemParam" paramName="applicationBaseURL" />do/jprss/Rss/Feed/show.action?id=${channel.id}&amp;lang=${channelLang.code}"
 						class="badge">${channelLang.descr}</a>
 				</li>
