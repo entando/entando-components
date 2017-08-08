4d7072cc965fc202f9e147a2c019b6e117a7e06e
Alessio Scalici <alessio.scalici@gmail.com>
2017-08-08 17:05:05 +0200

    inspinia html template

--- ui-bundles/themes/entando-page-inspinia/src/main/resources/sql/bundles/entando-page-inspinia/port_data_production.sql
+++ ui-bundles/themes/entando-page-inspinia/src/main/resources/sql/bundles/entando-page-inspinia/port_data_production.sql
@@ -1,226 +1,264 @@
 INSERT INTO pagemodels (code, descr, frames, plugincode, templategui) VALUES ('entando-page-inspinia', 'Inspinia - BPM layout', '<frames>
 	<frame pos="0">
 		<descr>Sidebar 1</descr>
-		<sketch x1="0" x2="1" y1="0" y2="0" />
+		<sketch x1="0" y1="0" x2="1" y2="0" />
 	</frame>
 	<frame pos="1">
 		<descr>Header</descr>
-		<sketch x1="2" x2="11" y1="0" y2="0" />
+		<sketch x1="2" y1="0" x2="11" y2="0" />
 	</frame>
 	<frame pos="2">
 		<descr>Sidebar 2</descr>
-		<sketch x1="0" x2="1" y1="1" y2="1" />
+		<sketch x1="0" y1="1" x2="1" y2="1" />
 	</frame>
 	<frame pos="3">
 		<descr>Content Left 1</descr>
-		<sketch x1="2" x2="4" y1="1" y2="1" />
+		<sketch x1="2" y1="1" x2="4" y2="1" />
 	</frame>
 	<frame pos="4">
 		<descr>Content Central 1</descr>
-		<sketch x1="5" x2="8" y1="1" y2="1" />
+		<sketch x1="5" y1="1" x2="8" y2="1" />
 	</frame>
 	<frame pos="5">
 		<descr>Content Right 1</descr>
-		<sketch x1="9" x2="11" y1="1" y2="1" />
+		<sketch x1="9" y1="1" x2="11" y2="1" />
 	</frame>
 	<frame pos="6">
 		<descr>Sidebar 3</descr>
-		<sketch x1="0" x2="1" y1="2" y2="2" />
+		<sketch x1="0" y1="2" x2="1" y2="2" />
 	</frame>
 	<frame pos="7">
 		<descr>Content Left 2</descr>
-		<sketch x1="2" x2="4" y1="2" y2="2" />
+		<sketch x1="2" y1="2" x2="4" y2="2" />
 	</frame>
 	<frame pos="8">
 		<descr>Content Central 2</descr>
-		<sketch x1="5" x2="8" y1="2" y2="2" />
+		<sketch x1="5" y1="2" x2="8" y2="2" />
 	</frame>
 	<frame pos="9">
 		<descr>Content Right 2</descr>
-		<sketch x1="9" x2="11" y1="2" y2="2" />
+		<sketch x1="9" y1="2" x2="11" y2="2" />
 	</frame>
 	<frame pos="10">
 		<descr>Sidebar 4</descr>
-		<sketch x1="0" x2="1" y1="3" y2="3" />
+		<sketch x1="0" y1="3" x2="1" y2="3" />
 	</frame>
 	<frame pos="11">
 		<descr>Content Full 1</descr>
-		<sketch x1="2" x2="11" y1="3" y2="3" />
+		<sketch x1="2" y1="3" x2="11" y2="3" />
 	</frame>
 	<frame pos="12">
 		<descr>Sidebar 5</descr>
-		<sketch x1="0" x2="1" y1="4" y2="4" />
+		<sketch x1="0" y1="4" x2="1" y2="4" />
 	</frame>
 	<frame pos="13">
 		<descr>Content Full 2</descr>
-		<sketch x1="2" x2="11" y1="4" y2="4" />
-	</frame>	
+		<sketch x1="2" y1="4" x2="11" y2="4" />
+	</frame>
 	<frame pos="14">
 		<descr>Sidebar 6</descr>
-		<sketch x1="0" x2="1" y1="5" y2="5" />
+		<sketch x1="0" y1="5" x2="1" y2="5" />
 	</frame>
 	<frame pos="15">
 		<descr>Content Left 3</descr>
-		<sketch x1="2" x2="4" y1="5" y2="5" />
+		<sketch x1="2" y1="5" x2="4" y2="5" />
 	</frame>
 	<frame pos="16">
 		<descr>Content Central 3</descr>
-		<sketch x1="5" x2="8" y1="5" y2="5" />
+		<sketch x1="5" y1="5" x2="8" y2="5" />
 	</frame>
 	<frame pos="17">
 		<descr>Content Right 3</descr>
-		<sketch x1="9" x2="11" y1="5" y2="5" />
+		<sketch x1="9" y1="5" x2="11" y2="5" />
+	</frame>
+	<frame pos="18">
+		<descr>Content Left 4</descr>
+		<sketch x1="2" y1="6" x2="4" y2="6" />
+	</frame>
+	<frame pos="19">
+		<descr>Content Central 4</descr>
+		<sketch x1="5" y1="6" x2="8" y2="6" />
+	</frame>
+	<frame pos="20">
+		<descr>Content Right 4</descr>
+		<sketch x1="9" y1="6" x2="11" y2="6" />
+	</frame>
+	<frame pos="21">
+		<descr>Footer</descr>
+		<sketch x1="2" y1="7" x2="11" y2="7" />
 	</frame>
 </frames>', NULL, '<#assign wp=JspTaglibs["/aps-core"]>
 <#assign c=JspTaglibs["http://java.sun.com/jsp/jstl/core"]>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title>
-            <@wp.currentPage param="title" /> - <@wp.i18n key="PORTAL_TITLE" />
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <link rel="icon" href="<@wp.info key="systemParam" paramName="applicationBaseURL" />favicon.png" type="image/png" />
-              <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-              <!--[if lt IE 9]>
-                      <script src="<@wp.resourceURL />static/js/entando-misc-html5-essentials/html5shiv.js"></script>
-              <![endif]-->
-             
-	      <@c.import url="/WEB-INF/aps/jsp/models/inc/content_inline_editing.jsp" />
-              <@c.import url="/WEB-INF/aps/jsp/models/inc/header-inclusions.jsp" />
-    </head>
-    <body>
-        <div class="navbar navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container">
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </a>
-                    <a class="brand" href="#"><img src="<@wp.imgURL />entando-logo.png" alt="Entando - Access. Build. Connect." /></a>
-                    <div class="nav-collapse collapse">
-                        <@wp.show frame=0 />
-                        <@wp.show frame=1 />
-                        <@wp.show frame=2 />
-                        <@wp.show frame=3 />
-                    </div><!-- /.nav-collapse -->
-                </div>
-            </div><!-- /navbar-inner -->
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="span12">
-                    <@wp.show frame=4 />
-                </div>
+   <head>
+      <meta charset="utf-8" />
+      <title>
+         <@wp.currentPage param="title" /> - <@wp.i18n key="PORTAL_TITLE" />
+      </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <link rel="icon" href="<@wp.info key="systemParam" paramName="applicationBaseURL" />
+      favicon.png" type="image/png" />
+      <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+      <!--[if lt IE 9]>
+      <script src="<@wp.resourceURL />static/js/entando-misc-html5-essentials/html5shiv.js"></script>
+      <![endif]-->
+      <@c.import url="/WEB-INF/aps/jsp/models/inc/content_inline_editing.jsp" />
+      <@c.import url="/WEB-INF/aps/jsp/models/inc/header-inclusions.jsp" />
+   </head>
+   <body>
+      <div id="wrapper">
+      <nav class="navbar-default navbar-static-side" role="navigation">
+         <div class="sidebar-collapse">
+            <div><@wp.show frame=0 /></div>
+            <div><@wp.show frame=2 /></div>
+            <div><@wp.show frame=6 /></div>
+            <div><@wp.show frame=10 /></div>
+            <div><@wp.show frame=12 /></div>
+            <div><@wp.show frame=14 /></div>
+         </div>
+      </nav>
+      <div id="page-wrapper" class="gray-bg dashbard-1">
+         <div class="row border-bottom">
+            <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+               <div class="navbar-header">
+                  <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+               </div>
+               <ul class="nav navbar-top-links navbar-right">
+                  <li class="dropdown">
+                     <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+                     <i class="fa fa-bell"></i>  <span class="label label-primary">8</span>
+                     </a>
+                     <ul class="dropdown-menu dropdown-alerts">
+                        <li>
+                           <a href="mailbox.html">
+                              <div>
+                                 <i class="fa fa-envelope fa-fw"></i> You have 16 messages
+                                 <span class="pull-right text-muted small">4 minutes ago</span>
+                              </div>
+                           </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                           <a href="profile.html">
+                              <div>
+                                 <i class="fa fa-twitter fa-fw"></i> 3 New Followers
+                                 <span class="pull-right text-muted small">12 minutes ago</span>
+                              </div>
+                           </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                           <a href="grid_options.html">
+                              <div>
+                                 <i class="fa fa-upload fa-fw"></i> Server Rebooted
+                                 <span class="pull-right text-muted small">4 minutes ago</span>
+                              </div>
+                           </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                           <div class="text-center link-block">
+                              <a href="#">
+                              <strong>See All Alerts</strong>
+                              <i class="fa fa-angle-right"></i>
+                              </a>
+                           </div>
+                        </li>
+                     </ul>
+                  </li>
+                  <li>
+                     <a href="#">
+                     <i class="fa fa-sign-out"></i> Log out
+                     </a>
+                  </li>
+               </ul>
+            </nav>
+         </div>
+         <div class="row white-bg" style="padding-bottom:10px; border-bottom:2px solid #e7eaec;">
+            <@wp.show frame=1 />
+         </div>
+         <div style="padding-top:20px;">
+            <div class="col-md-4">
+               <div class="white-bg">
+                  <@wp.show frame=3 />
+               </div>
             </div>
-            <div class="row">
-                <@wp.show frame=5 />
+            <div class="col-md-4">
+               <div class="white-bg">
+                  <@wp.show frame=4 />
+               </div>
             </div>
-            <div class="row">
-                <div class="span12">
-                    <@wp.show frame=6 />
-                </div>
+            <div class="col-md-4">
+               <div class="white-bg">
+                  <@wp.show frame=5 />
+               </div>
             </div>
-            <div class="row">
-                <div class="span12">
-                    <@wp.show frame=7 />
-                </div>
+         </div>
+         <div style="padding-top:20px;">
+            <div class="col-md-4">
+               <div class="white-bg">
+                  <@wp.show frame=7 />
+               </div>
             </div>
-            <div class="row">
-                <div class="span4">
-                    <@wp.show frame=8 />
-                </div>
-                <div class="span4">
-                    <@wp.show frame=9 />
-                </div>
-                <div class="span4">
-                    <@wp.show frame=10 />
-                </div>
+            <div class="col-md-4">
+               <div class="white-bg">
+                  <@wp.show frame=8 />
+               </div>
             </div>
-            <div class="row">
-                <div class="span6">
-                    <@wp.show frame=11 />
-                </div>
-                <div class="span6">
-                    <@wp.show frame=12 />
-                </div>
+            <div class="col-md-4">
+               <div class="white-bg">
+                  <@wp.show frame=9 />
+               </div>
             </div>
-            <div class="row">
-                <div class="span6">
-                    <@wp.show frame=13 />
-                </div>
-                <div class="span6">
-                    <@wp.show frame=14 />
-                </div>
+         </div>
+         <div class="row">
+            <div class="col-lg-12">
+               <div class="wrapper wrapper-content">
+                  <div class="row">
+                     <div class="col-lg-12">
+                        <@wp.show frame=11 />
+                     </div>
+                  </div>
+                  <div class="row">
+                     <div class="col-lg-12">
+                        <@wp.show frame=13 />
+                     </div>
+                  </div>
+                  <div class="row">
+                     <div class="col-lg-4">
+                        <div class="white-bg">
+                           <@wp.show frame=15 />
+                        </div>
+                        <div class="white-bg">
+                           <@wp.show frame=18 />
+                        </div>
+                     </div>
+                     <div class="col-lg-4">
+                        <div class="white-bg">
+                           <@wp.show frame=16 />
+                        </div>
+                        <div class="white-bg">
+                           <@wp.show frame=19 />
+                        </div>
+                     </div>
+                     <div class="col-lg-4">
+                        <div class="white-bg">
+                           <@wp.show frame=17 />
+                        </div>
+                        <div class="white-bg">
+                           <@wp.show frame=20 />
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="footer">
+                  <@wp.show frame=21 />
+               </div>
             </div>
-            <div class="row">
-                <div class="span12">
-                    <@wp.show frame=15 />
-                </div>
-            </div>
-            <div class="row">
-                <div class="span12">
-                    <@wp.show frame=16 />
-                </div>
-            </div>
-            <div class="row">
-                <div class="span12">
-                    <@wp.show frame=17 />
-                </div>
-            </div>
-            <div class="row">
-                <div class="span12">
-                    
-                </div>
-            </div>
-            <div class="row">
-                <div class="span12">
-                    
-                </div>
-            </div>
-            <div class="row">
-                <div class="span6">
-                    
-                </div>
-                <div class="span6">
-                    
-                </div>
-            </div>
-            <div class="row">
-                <div class="span6">
-                    
-                </div>
-                <div class="span6">
-                    
-                </div>
-            </div>
-        </div>
-        <footer class="padding-medium-top">
-            <div class="container">
-                <div class="row margin-medium-bottom">
-                    <div class="span12">
-                        
-                    </div>
-                </div>
-                <div class="row margin-medium-bottom">
-                    <div class="span4">
-                       
-                    </div>
-                    <div class="span4">
-                        
-                    </div>
-                    <div class="span4">
-                        
-                    </div>
-                </div>
-                <div class="row">
-                    <p class="span12 text-center margin-medium-top"><@wp.i18n key="COPYRIGHT" escapeXml=false /> - Powered by <a href="http://www.entando.com/">Entando - Access. Build. Connect.</a></p>
-                </div>
-            </div>
-        </footer>
-    </body>
+         </div>
+      </div>
+   </body>
 </html>');
