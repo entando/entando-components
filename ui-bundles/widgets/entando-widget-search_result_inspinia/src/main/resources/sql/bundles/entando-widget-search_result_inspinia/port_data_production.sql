5ba02d3153c03001cb50b9bca949844f5caefe27
Alessio Scalici <alessio.scalici@gmail.com>
2017-08-11 16:04:33 +0200

    search form

---
+++ ui-bundles/widgets/entando-widget-search_result_inspinia/src/main/resources/sql/bundles/entando-widget-search_result_inspinia/port_data_production.sql
@@ -0,0 +1,34 @@
+INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('entando-widget-search_result_inspinia', '<?xml version="1.0" encoding="UTF-8"?>
+<properties>
+<property key="en">Search Result</property>
+<property key="it">Search Result</property>
+</properties>', NULL, NULL, NULL, NULL, 1, NULL);
+
+INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('entando-widget-search_result_inspinia', 'entando-widget-search_result_inspinia', NULL, NULL, '<#assign jacms=JspTaglibs["/jacms-aps-core"]>
+<#assign wp=JspTaglibs["/aps-core"]>
+<h1><@wp.i18n key="SEARCH_RESULTS" /></h1>
+<#if (RequestParameters.search?? && RequestParameters.search!='')>
+<@jacms.searcher listName="contentListResult" />
+</#if>
+<p><@wp.i18n key="SEARCHED_FOR" />: <em><strong><#if (RequestParameters.search??)>${RequestParameters.search}</#if></strong></em></p>
+<#if (contentListResult??) && (contentListResult?has_content) && (contentListResult?size > 0)>
+<@wp.pager listName="contentListResult" objectName="groupContent" max=10 pagerIdFromFrame=true advanced=true offset=5>
+	<@wp.freemarkerTemplateParameter var="group" valueName="groupContent" removeOnEndTag=true >
+	<p><em><@wp.i18n key="SEARCH_RESULTS_INTRO" /> <!-- infamous whitespace hack -->
+	${groupContent.size}
+	<@wp.i18n key="SEARCH_RESULTS_OUTRO" /> [${groupContent.begin + 1} &ndash; ${groupContent.end + 1}]:</em></p>
+	<@wp.fragment code="default_pagerBlock" escapeXml=false />
+	<#list contentListResult as contentId>
+	<#if (contentId_index >= groupContent.begin) && (contentId_index <= groupContent.end)>
+		<@jacms.content contentId="${contentId}" modelId="list" />
+	</#if>
+	</#list>
+	<@wp.fragment code="default_pagerBlock" escapeXml=false />
+	</@wp.freemarkerTemplateParameter>
+</@wp.pager>
+<#else>
+<p class="alert alert-info"><@wp.i18n key="SEARCH_NOTHING_FOUND" /></p>
+</#if>
+', 1);
+
+
