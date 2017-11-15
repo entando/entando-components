4924d35f4d7e1bfcfa59c2c2c7673aae170cb5fc
Federico Locci <f.locci@entando.com>
2017-11-15 17:37:02 +0100

    refacturing & rename bpmDatatable in bpmTaskListDatatable

--- plugins/entando-plugin-jpkiebpm/src/main/db/update/scriptPort.sql
+++ plugins/entando-plugin-jpkiebpm/src/main/db/update/scriptPort.sql
@@ -9,18 +9,11 @@ INSERT INTO widgetcatalog VALUES ('bpm-datatable-task-list', '<?xml version="1.0
 	<parameter name="widgetInfoId">WidgetInfoID</parameter>
 	<action name="jpkiebpmBpmTaskListDatatableWidgetViewerConfig"/>
 </config>', 'jpkiebpm', NULL, NULL, 1, NULL);
-<<<<<<< HEAD
+
 INSERT INTO widgetcatalog VALUES ('bpm-datatable-process-list', '<?xml version="1.0" encoding="UTF-8"?>
 <properties>
 <property key="en">BPM-Datatable Process list</property>
 <property key="it">BPM-Datatable Lista processi</property>
-=======
-
-INSERT INTO widgetcatalog VALUES ('bpm-datatable-process-list', '<?xml version="1.0" encoding="UTF-8"?>
-<properties>
-<property key="en">BPM-Process list</property>
-<property key="it">BPM-Lista processi</property>
->>>>>>> 96671f7fc41e2f1cbce82746e124f04cd5be341a
 </properties>', '<config>
 	<parameter name="widgetInfoId">WidgetInfoID</parameter>
 	<action name="jpkiebpmBpmProcessDatatableWidgetViewerConfig"/>
