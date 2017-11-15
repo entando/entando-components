96671f7fc41e2f1cbce82746e124f04cd5be341a
Federico Locci <federico.locci@gmail.com>
2017-11-15 16:14:44 +0100

    Process datatable config (#166)
    * Restapi compliancy send signal (#160)
    
    * components: included lo4j2 config
    
    * jprestapi: added option for timout on single connection basis, or overall timout setup
    
    * jpkiebpm: added overall timeout option, implemented signals to the KIE server, refactored tests
    
    * jpkiebpm: refactored getHumanTaskList REST API
    
    * jpkiebpm: added delete process API
    
    * jpkiebpm: timeout conf. added
    
    * Cosmetic changes
    
    * send singnal api
    
    * jpkiebpm: start new process
    
    * jpkiebpm: fix start new process
    
    * jpkiebpm: added task state update
    
    * jpkiebpm: fixed kieFormNagaerInterface
    
    * kie widget config fix and start process api
    
    * Update ApiTaskInterface.java
    
    Removed comment
    
    * jpkiebpm: cosmetic changes
    
    * jpkiebpm: start process fix
    
    * jpkiebpm: start process - print process id
    
    * jpkiebpm: introduced widget for process list (not completed)
    
    * jpkiebpm:added process list REST endpoint, misc. refactor
    
    * jpkiebpm: added process list REST
    
    * jpkiebpm: fixed method refactored
    
    * spring config fix
    
    * form-updated
    
    * refact datatable (#161)
    
    * refact datatable
    
    * refact datatable fixed
    
    * refact for columnDefinition (#163)
    
    * refact datatable
    
    * refact datatable fixed
    
    * refact datatable fixed
    
    * add scriptPort.sql
    
    * merge from master
    
    * merge from master
    
    * merge from master
    
    * fix custom-data-table.js
    
    * fix KieProcessInstance.java
    
    * refacturing & rename bpmDatatable in bpmTaskListDatatable

--- plugins/entando-plugin-jpkiebpm/src/main/db/update/scriptPort.sql
+++ plugins/entando-plugin-jpkiebpm/src/main/db/update/scriptPort.sql
@@ -1,4 +1,5 @@
 DELETE FROM widgetcatalog WHERE code ='bpm-datatable';
+DELETE FROM widgetcatalog WHERE code ='bpm-datatable-task-list';
 
 INSERT INTO widgetcatalog VALUES ('bpm-datatable-task-list', '<?xml version="1.0" encoding="UTF-8"?>
 <properties>
@@ -6,8 +7,9 @@ INSERT INTO widgetcatalog VALUES ('bpm-datatable-task-list', '<?xml version="1.0
 <property key="it">BPM-Datatable Task List</property>
 </properties>', '<config>
 	<parameter name="widgetInfoId">WidgetInfoID</parameter>
-	<action name="jpkiebpmBpmDatatableWidgetViewerConfig"/>
+	<action name="jpkiebpmBpmTaskListDatatableWidgetViewerConfig"/>
 </config>', 'jpkiebpm', NULL, NULL, 1, NULL);
+
 INSERT INTO widgetcatalog VALUES ('bpm-datatable-process-list', '<?xml version="1.0" encoding="UTF-8"?>
 <properties>
 <property key="en">BPM-Process list</property>
--- plugins/entando-plugin-jpkiebpm/src/main/java/org/entando/entando/plugins/jpkiebpm/apsadmin/portal/specialwidget/BpmDatatableWidgetAction.java
+++ plugins/entando-plugin-jpkiebpm/src/main/java/org/entando/entando/plugins/jpkiebpm/apsadmin/portal/specialwidget/BpmDatatableWidgetAction.java
@@ -3,195 +3,36 @@ package org.entando.entando.plugins.jpkiebpm.apsadmin.portal.specialwidget;
 import com.agiletec.aps.system.exception.ApsSystemException;
 import com.agiletec.aps.system.services.group.Group;
 import com.agiletec.aps.system.services.group.IGroupManager;
-import com.agiletec.aps.system.services.page.IPage;
 import com.agiletec.aps.system.services.page.Widget;
 import com.agiletec.aps.util.ApsProperties;
 import org.apache.commons.lang3.StringUtils;
 import org.entando.entando.aps.system.services.widgettype.WidgetTypeParameter;
 import org.entando.entando.plugins.jpkiebpm.aps.system.services.bpmwidgetinfo.BpmWidgetInfo;
-import org.entando.entando.plugins.jpkiebpm.aps.system.services.bpmwidgetinfo.IBpmWidgetInfoManager;
-import org.entando.entando.plugins.jpkiebpm.aps.system.services.kie.IKieFormManager;
-import org.entando.entando.plugins.jpkiebpm.aps.system.services.kie.IKieFormOverrideManager;
-import org.entando.entando.plugins.jpkiebpm.aps.system.services.kie.model.KieTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Serializable;
 import java.util.*;
 
 /**
  *
  */
-public class BpmDatatableWidgetAction extends BpmFormWidgetAction {
+public abstract class BpmDatatableWidgetAction extends BpmFormWidgetAction implements Serializable {
 
     private static final Logger _logger = LoggerFactory.getLogger(BpmDatatableWidgetAction.class);
-    private IKieFormManager formManager;
-    private IKieFormOverrideManager kieFormOverrideManager;
-    private IBpmWidgetInfoManager bpmWidgetInfoManager;
-    private IGroupManager groupManager;
-    private String groups;
-    private List<String> selectedGroups = new ArrayList<>();
-    private final List<String> checkedField = new ArrayList<>();
-    private final List<FieldDatatable> fieldsDatatable = new ArrayList<>();
-    private final String PROP_BPM_GROUP = "groups";
-    private final String PREFIX_BPM_GROUP = "bpm_";
-    private final String PREFIX_FIELD = "field_";
-    private final String PREFIX_VISIBLE = "visible_";
-    private final String PREFIX_OVERRIDE = "override_";
 
-    private final String PREFIX_POSITION = "position_";
+    protected List<String> selectedGroups = new ArrayList<>();
+    protected final List<String> checkedField = new ArrayList<>();
+    protected final List<FieldDatatable> fieldsDatatable = new ArrayList<>();
+    protected final String PROP_BPM_GROUP = "groups";
+    protected final String PREFIX_BPM_GROUP = "bpm_";
+    protected final String PREFIX_FIELD = "field_";
+    protected final String PREFIX_VISIBLE = "visible_";
+    protected final String PREFIX_OVERRIDE = "override_";
+    protected final String PREFIX_POSITION = "position_";
+    protected String groups;
+    protected IGroupManager groupManager;
 
-
-    @Override
-    protected void loadWidgetInfo() {
-        _logger.info("Method loadWidgetInfo");
-        try {
-            String widgetInfoId = this.getWidget().getConfig().getProperty(PROP_NAME_WIDGET_INFO_ID);
-            if (StringUtils.isNotBlank(widgetInfoId)) {
-                BpmWidgetInfo widgetInfo = this.getBpmWidgetInfoManager().getBpmWidgetInfo(Integer.valueOf(widgetInfoId));
-                if (null != widgetInfo) {
-
-                    this.initSharedParameters(widgetInfo);
-
-                    String groups = widgetInfo.getConfigDraft().getProperty(PROP_BPM_GROUP);
-                    if (StringUtils.isNotBlank(groups) && !groups.equals("null")) {
-                        String[] idStrings = groups.split(",");
-                        for (String string : idStrings) {
-                            this.selectedGroups.add(string.trim());
-                        }
-                    }
-
-                    for (final String prop : widgetInfo.getConfigDraft().stringPropertyNames()) {
-                        if (prop.startsWith(PREFIX_FIELD)) {
-
-                            final FieldDatatable fd = new FieldDatatable(prop.replace(PREFIX_FIELD, "").trim());
-                            fd.setField(PREFIX_FIELD + fd.getName());
-                            fd.setPosition(Byte.parseByte(widgetInfo.getConfigDraft().getProperty(PREFIX_POSITION + fd.getName())));
-                            fd.setVisible(Boolean.valueOf(widgetInfo.getConfigDraft().getProperty(PREFIX_VISIBLE + fd.getName())));
-                            fd.setOverride(widgetInfo.getConfigDraft().getProperty(PREFIX_OVERRIDE + fd.getName()));
-
-                            this.fieldsDatatable.add(fd);
-                        }
-
-                    }
-
-                    Collections.sort(this.fieldsDatatable, new Comparator<FieldDatatable>() {
-                        @Override
-                        public int compare(FieldDatatable o1, FieldDatatable o2) {
-                            return o1.position.compareTo(o2.position);
-                        }
-                    });
-                }
-
-            }
-
-        } catch (ApsSystemException e) {
-            _logger.error("Error loading WidgetInfo", e);
-        }
-    }
-
-    private void loadFieldIntoDatatableFromBpm() throws ApsSystemException {
-        HashMap<String, String> opt = new HashMap<String, String>();
-        List<KieTask> task = formManager.getHumanTaskList(null, opt);
-
-        if (!task.isEmpty()) {
-
-            StringTokenizer tokenizer = new StringTokenizer(task.get(0).toString(), ",");
-            Byte position = 1;
-            while (tokenizer.hasMoreTokens()) {
-                final String name = tokenizer.nextToken().trim();
-                final FieldDatatable fd = new FieldDatatable(name);
-                fd.setField(PREFIX_FIELD + name);
-                fd.setPosition(position++);
-                fd.setVisible(Boolean.valueOf(true));
-                fd.setOverride("");
-                this.fieldsDatatable.add(fd);
-
-            }
-        }
-    }
-
-    private void setPropertiesIntoWidgetInfo(final ApsProperties properties, final String procId, final String contId) {
-        properties.put(PROP_PROCESS_ID, procId);
-        properties.put(PROP_CONTAINER_ID, contId);
-        if (this.getGroups() != null) {
-            properties.put(PROP_BPM_GROUP, this.getGroups());
-        }
-        for (final FieldDatatable field : fieldsDatatable) {
-            properties.put(PREFIX_FIELD + field.getName(), field.getField());
-            properties.put(PREFIX_VISIBLE + field.getName(), Boolean.toString(field.getVisible()));
-            properties.put(PREFIX_OVERRIDE + field.getName(), field.getOverride());
-            properties.put(PREFIX_POSITION + field.getName(), Byte.toString(field.getPosition()));
-
-        }
-    }
-
-    protected BpmWidgetInfo storeWidgetInfo(final Widget widget) {
-        BpmWidgetInfo widgetInfo = null;
-        try {
-            widgetInfo = new BpmWidgetInfo();
-            widgetInfo.setFramePosDraft(this.getFrame());
-            widgetInfo.setPageCode(this.getPageCode());
-            final String[] param = this.getProcessPath().split("@");
-            final String procId = param[0];
-            final String contId = param[1];
-            widgetInfo.setWidgetType(widget.getType().getCode());
-            ApsProperties properties = new ApsProperties();
-            this.setPropertiesIntoWidgetInfo(properties, procId, contId);
-            widgetInfo.setInformationDraft(properties.toXml());
-            this.updateConfigWidget(widgetInfo,widget);
-        } catch (Exception e) {
-            _logger.error("Error in save WidgetInfo", e);
-
-        }
-        return widgetInfo;
-    }
-
-    protected void createValuedShowlet() throws Exception {
-        _logger.info("Method createValuedShowlet");
-        for (final Enumeration enumeration = this.getRequest().getParameterNames(); enumeration.hasMoreElements(); ) {
-            final String key = (String) enumeration.nextElement();
-
-            if (key.startsWith(PREFIX_FIELD)) {
-                this.fieldsDatatable.add(new FieldDatatable(key.replace(PREFIX_FIELD, "").trim()));
-            }
-        }
-        for (final FieldDatatable field : fieldsDatatable) {
-
-
-            String keyParameter = PREFIX_FIELD + field.getName();
-            field.setField(this.getRequest().getParameter(keyParameter));
-
-            keyParameter = PREFIX_VISIBLE + field.getName();
-            field.setVisible(this.getRequest().getParameter(keyParameter) != null);
-
-            keyParameter = PREFIX_OVERRIDE + field.getName();
-            field.setOverride(this.getRequest().getParameter(keyParameter));
-
-            keyParameter = PREFIX_POSITION + field.getName();
-            String value = this.getRequest().getParameter(keyParameter);
-            field.setPosition(Byte.decode(value));
-        }
-
-        final Widget widget = this.createNewWidget();
-        List<WidgetTypeParameter> parameters = widget.getType().getTypeParameters();
-        super.setPropertyWidget(parameters, widget);
-        BpmWidgetInfo widgetInfo = this.storeWidgetInfo(widget);
-        widget.getConfig().setProperty(PROP_NAME_WIDGET_INFO_ID, String.valueOf(widgetInfo.getId()));
-        this.setWidget(widget);
-
-
-    }
-
-
-    public String chooseForm() {
-        try {
-            this.loadFieldIntoDatatableFromBpm();
-        } catch (Throwable t) {
-            _logger.error("Error in chooseForm", t);
-            return FAILURE;
-        }
-        return SUCCESS;
-    }
 
     public String getGroups() {
         return groups;
@@ -210,13 +51,13 @@ public class BpmDatatableWidgetAction extends BpmFormWidgetAction {
     }
 
     public List<String> getListBpmGroups() {
-        final List<String> groups = new ArrayList<>();
+        final List<String> listGroups = new ArrayList<>();
         for (final Group gr : this.groupManager.getGroups()) {
             if (gr.getName().startsWith(PREFIX_BPM_GROUP)) {
-                groups.add(gr.getName().replace(PREFIX_BPM_GROUP, "").trim());
+                listGroups.add(gr.getName().replace(PREFIX_BPM_GROUP, "").trim());
             }
         }
-        return groups;
+        return listGroups;
 
 
     }
@@ -242,100 +83,150 @@ public class BpmDatatableWidgetAction extends BpmFormWidgetAction {
         return this.fieldsDatatable;
     }
 
-    @Override
-    public IGroupManager getGroupManager() {
-        return groupManager;
+
+    protected abstract void loadFieldIntoDatatableFromBpm() throws ApsSystemException;
+
+    protected void loadDataIntoFieldDatatable(List element) {
+
+        StringTokenizer tokenizer = new StringTokenizer(element.get(0).toString(), ",");
+        Byte position = 1;
+        while (tokenizer.hasMoreTokens()) {
+            final String name = tokenizer.nextToken().trim();
+            final FieldDatatable fd = new FieldDatatable(name);
+            fd.setField(PREFIX_FIELD + name);
+            fd.setPosition(position++);
+            fd.setVisible(Boolean.valueOf(true));
+            fd.setOverride("");
+            this.fieldsDatatable.add(fd);
+
+        }
+    }
+
+    protected void setPropertiesIntoWidgetInfo(final ApsProperties properties, final String procId, final String contId) {
+        properties.put(PROP_PROCESS_ID, procId);
+        properties.put(PROP_CONTAINER_ID, contId);
+        if (this.getGroups() != null) {
+            properties.put(PROP_BPM_GROUP, this.getGroups());
+        }
+        for (final FieldDatatable field : fieldsDatatable) {
+            properties.put(PREFIX_FIELD + field.getName(), field.getField());
+            properties.put(PREFIX_VISIBLE + field.getName(), Boolean.toString(field.getVisible()));
+            properties.put(PREFIX_OVERRIDE + field.getName(), field.getOverride());
+            properties.put(PREFIX_POSITION + field.getName(), Byte.toString(field.getPosition()));
+
+        }
     }
 
     @Override
-    public void setGroupManager(IGroupManager groupManager) {
-        this.groupManager = groupManager;
-    }
+    protected BpmWidgetInfo storeWidgetInfo(final Widget widget) {
+        BpmWidgetInfo widgetInfo = null;
+        try {
+            widgetInfo = new BpmWidgetInfo();
+            widgetInfo.setFramePosDraft(this.getFrame());
+            widgetInfo.setPageCode(this.getPageCode());
+            final String[] param = this.getProcessPath().split("@");
+            final String procId = param[0];
+            final String contId = param[1];
+            widgetInfo.setWidgetType(widget.getType().getCode());
+            ApsProperties properties = new ApsProperties();
+            this.setPropertiesIntoWidgetInfo(properties, procId, contId);
+            widgetInfo.setInformationDraft(properties.toXml());
+            this.updateConfigWidget(widgetInfo, widget);
+        } catch (Exception e) {
+            _logger.error("Error in save WidgetInfo", e);
 
-
-    @Override
-    public IKieFormManager getFormManager() {
-        return formManager;
-    }
-
-    @Override
-    public void setFormManager(IKieFormManager formManager) {
-        this.formManager = formManager;
-    }
-
-    @Override
-    public IBpmWidgetInfoManager getBpmWidgetInfoManager() {
-        return bpmWidgetInfoManager;
-    }
-
-    @Override
-    public void setBpmWidgetInfoManager(IBpmWidgetInfoManager bpmWidgetInfoManager) {
-        this.bpmWidgetInfoManager = bpmWidgetInfoManager;
+        }
+        return widgetInfo;
     }
 
     @Override
-    public IKieFormOverrideManager getKieFormOverrideManager() {
-        return kieFormOverrideManager;
+    public String chooseForm() {
+        try {
+            this.loadFieldIntoDatatableFromBpm();
+        } catch (Exception t) {
+            _logger.error("Error in chooseForm", t);
+            return FAILURE;
+        }
+        return SUCCESS;
     }
 
     @Override
-    public void setKieFormOverrideManager(IKieFormOverrideManager kieFormOverrideManager) {
-        this.kieFormOverrideManager = kieFormOverrideManager;
+    protected void createValuedShowlet() throws Exception {
+        _logger.info("Method createValuedShowlet");
+        for (final Enumeration enumeration = this.getRequest().getParameterNames(); enumeration.hasMoreElements(); ) {
+            final String key = (String) enumeration.nextElement();
+
+            if (key.startsWith(PREFIX_FIELD)) {
+                this.fieldsDatatable.add(new FieldDatatable(key.replace(PREFIX_FIELD, "").trim()));
+            }
+        }
+        for (final FieldDatatable field : fieldsDatatable) {
+
+            String keyParameter = PREFIX_FIELD + field.getName();
+            field.setField(this.getRequest().getParameter(keyParameter));
+
+            keyParameter = PREFIX_VISIBLE + field.getName();
+            field.setVisible(this.getRequest().getParameter(keyParameter) != null);
+
+            keyParameter = PREFIX_OVERRIDE + field.getName();
+            field.setOverride(this.getRequest().getParameter(keyParameter));
+
+            keyParameter = PREFIX_POSITION + field.getName();
+            String value = this.getRequest().getParameter(keyParameter);
+            field.setPosition(Byte.decode(value));
+        }
+
+        final Widget widget = this.createNewWidget();
+        List<WidgetTypeParameter> parameters = widget.getType().getTypeParameters();
+        super.setPropertyWidget(parameters, widget);
+        BpmWidgetInfo widgetInfo = this.storeWidgetInfo(widget);
+        widget.getConfig().setProperty(PROP_NAME_WIDGET_INFO_ID, String.valueOf(widgetInfo.getId()));
+        this.setWidget(widget);
+
     }
 
+    @Override
+    protected void loadWidgetInfo() {
+        try {
+            final String widgetInfoId = this.getWidget().getConfig().getProperty(PROP_NAME_WIDGET_INFO_ID);
+            if (StringUtils.isNotBlank(widgetInfoId)) {
+                BpmWidgetInfo widgetInfo = this.getBpmWidgetInfoManager().getBpmWidgetInfo(Integer.valueOf(widgetInfoId));
+                this.initSharedParameters(widgetInfo);
+                String groups = widgetInfo.getConfigDraft().getProperty(PROP_BPM_GROUP);
+                if (StringUtils.isNotBlank(groups) && !groups.equals("null")) {
+                    String[] idStrings = groups.split(",");
+                    for (String string : idStrings) {
+                        this.selectedGroups.add(string.trim());
+                    }
+                }
+                for (final String prop : widgetInfo.getConfigDraft().stringPropertyNames()) {
+                    if (prop.startsWith(PREFIX_FIELD)) {
+                        this.fieldsDatatable.add(this.getNewFieldDatatable(prop, widgetInfo));
+                    }
 
-    public static class FieldDatatable {
+                }
 
-        private String name;
-        private String field;
-        private Boolean visible;
-        private String override;
-        private Byte position;
+                Collections.sort(this.fieldsDatatable, new Comparator<FieldDatatable>() {
+                    @Override
+                    public int compare(FieldDatatable o1, FieldDatatable o2) {
+                        return o1.getPosition().compareTo(o2.getPosition());
+                    }
+                });
 
 
-        FieldDatatable(final String name) {
-            this.name = name;
+            }
 
+        } catch (ApsSystemException e) {
+            _logger.error("Error loading WidgetInfo", e);
         }
+    }
 
-        public String getField() {
-            return field;
-        }
-
-        public void setField(String field) {
-            this.field = field;
-        }
-
-        public Boolean getVisible() {
-            return visible;
-        }
-
-        public void setVisible(Boolean visible) {
-            this.visible = visible;
-        }
-
-        public String getOverride() {
-            return override;
-        }
-
-        public void setOverride(String override) {
-            this.override = override;
-        }
-
-        public Byte getPosition() {
-            return position;
-        }
-
-        public void setPosition(Byte position) {
-            this.position = position;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
+    private FieldDatatable getNewFieldDatatable(final String data, final BpmWidgetInfo widgetInfo) {
+        final FieldDatatable fd = new FieldDatatable(data.replace(PREFIX_FIELD, "").trim());
+        fd.setField(PREFIX_FIELD + fd.getName());
+        fd.setPosition(Byte.parseByte(widgetInfo.getConfigDraft().getProperty(PREFIX_POSITION + fd.getName())));
+        fd.setVisible(Boolean.valueOf(widgetInfo.getConfigDraft().getProperty(PREFIX_VISIBLE + fd.getName())));
+        fd.setOverride(widgetInfo.getConfigDraft().getProperty(PREFIX_OVERRIDE + fd.getName()));
+        return fd;
     }
 }
