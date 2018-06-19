edc5f8dc25092ba424babe75befa37ff1bbf7074
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-06-19 12:12:21 +0200

    EN-1604 add unit test

--- plugins/entando-plugin-jpseo/src/main/webapp/WEB-INF/plugins/jpseo/apsadmin/jsp/portal/pageSettings-jack.jsp
+++ plugins/entando-plugin-jpseo/src/main/webapp/WEB-INF/plugins/jpseo/apsadmin/jsp/portal/pageSettings-jack.jsp
@@ -3,48 +3,49 @@
 <%@ taglib uri="/apsadmin-core" prefix="wpsa" %>
 <%@ taglib uri="/apsadmin-form" prefix="wpsf" %>
 <div>
-<legend><s:text name="jpseo.legend.seoPageSettings" /></legend>
+    <legend><s:text name="jpseo.legend.seoPageSettings" /></legend>
 
-<div class="row">
-<%-- robot.txt --%>
-<s:set var="fieldErrorsVar" value="%{fieldErrors['robotContent']}" />
-<s:set var="hasFieldErrorVar" value="#fieldErrorsVar != null && !#fieldErrorsVar.isEmpty()" />
-<s:set var="controlGroupErrorClass" value="%{#hasFieldErrorVar ? ' has-error' : ''}" />
-<div class="form-group<s:property value="#controlGroupErrorClass" />">
-    <label class="col-sm-2 control-label" for="xmlConfig">
-        <s:text name="jpseo.label.robotContent" />
-    </label>
-    <div class="col-sm-10">
-        <wpsf:textarea cols="50" rows="5" name="robotContent" id="robotContent" value="%{#attr.robotContent}" cssClass="form-control" />
-        <s:if test="#hasFieldErrorVar">
-            <span class="help-block text-danger">
-                <s:iterator value="%{#fieldErrorsVar}"><s:property />&#32;</s:iterator>
-            </span>
-        </s:if>
+    <div class="row">
+        <%-- robot.txt --%>
+        <s:set var="fieldErrorsVar" value="%{fieldErrors['robotContent']}" />
+        <s:set var="hasFieldErrorVar" value="#fieldErrorsVar != null && !#fieldErrorsVar.isEmpty()" />
+        <s:set var="controlGroupErrorClass" value="%{#hasFieldErrorVar ? ' has-error' : ''}" />
+        <div class="form-group<s:property value="#controlGroupErrorClass" />">
+            <label class="col-sm-2 control-label" for="xmlConfig">
+                <s:text name="jpseo.label.robotContent" />
+            </label>
+            <div class="col-sm-10">
+                <wpsf:textarea cols="50" rows="5" name="robotContent" id="robotContent" value="%{#attr.robotContent}" cssClass="form-control" />
+                <s:if test="#hasFieldErrorVar">
+                    <span class="help-block text-danger">
+                        <s:iterator value="%{#fieldErrorsVar}"><s:property />&#32;</s:iterator>
+                    </span>
+                </s:if>
+            </div>
+        </div>
     </div>
-</div>
-</div>
 
-<div class="row">
-<%-- robotFilePath --%>
-<s:set var="fieldErrorsVar" value="%{fieldErrors['robotFilePath']}" />
-<s:set var="hasFieldErrorVar" value="#fieldErrorsVar != null && !#fieldErrorsVar.isEmpty()" />
-<s:set var="controlGroupErrorClass" value="%{#hasFieldErrorVar ? ' has-error' : ''}" />
+    <div class="row">
+        <%-- robotFilePath --%>
+        <s:set var="fieldErrorsVar" value="%{fieldErrors['robotFilePath']}" />
+        <s:set var="hasFieldErrorVar" value="#fieldErrorsVar != null && !#fieldErrorsVar.isEmpty()" />
+        <s:set var="controlGroupErrorClass" value="%{#hasFieldErrorVar ? ' has-error' : ''}" />
 
-<div class="form-group">
-    <label class="col-sm-2 control-label" for="robotFilePath">
-        <s:text name="jpseo.label.robotFilePath" />
-    </label>
-    <div class="col-sm-10">
-        <wpsf:textfield name="robotFilePath" id="robotFilePath" value="%{#attr.robotFilePath}" cssClass="form-control" />
-        <s:if test="#hasFieldErrorVar">
-            <span class="help-block text-danger">
-                <s:iterator value="%{#fieldErrorsVar}"><s:property />&#32;</s:iterator>
-            </span>
-        </s:if>
+        <div class="form-group">
+            <label class="col-sm-2 control-label" for="robotFilePath">
+                <s:text name="jpseo.label.robotFilePath" />
+            </label>
+            <div class="col-sm-10">
+                <wpsf:textfield name="robotFilePath" id="robotFilePath" value="%{#attr.robotFilePath}" cssClass="form-control" />
+                <s:if test="#hasFieldErrorVar">
+                    <span class="help-block text-danger">
+                        <s:iterator value="%{#fieldErrorsVar}"><s:property />&#32;</s:iterator>
+                    </span>
+                </s:if>
+            </div>
+        </div>
     </div>
+  
 </div>
-</div>
-        
-        
-</div>
+
+<br />
---
+++ plugins/entando-plugin-jpseo/src/test/java/org/entando/entando/plugins/jpseo/apsadmin/portal/TestPageSettingsAction.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2018-Present Entando Inc. (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package org.entando.entando.plugins.jpseo.apsadmin.portal;
+
+import java.util.Map;
+
+import com.agiletec.aps.system.SystemConstants;
+import com.agiletec.aps.system.services.baseconfig.ConfigInterface;
+import com.agiletec.apsadmin.ApsAdminBaseTestCase;
+import com.agiletec.apsadmin.portal.PageSettingsAction;
+import com.opensymphony.xwork2.Action;
+import java.io.File;
+import org.entando.entando.aps.system.services.storage.IStorageManager;
+import org.entando.entando.plugins.jpseo.aps.system.JpseoSystemConstants;
+
+/**
+ * @author E.Santoboni
+ */
+public class TestPageSettingsAction extends ApsAdminBaseTestCase {
+
+	private ConfigInterface configManager;
+	private IStorageManager storageManager;
+	private String oldConfigParam;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		this.init();
+	}
+    
+	public void testConfigSystemParams() throws Throwable {
+		this.setUserOnSession("admin");
+		this.initAction("/do/Page", "systemParams");
+		String result = this.executeAction();
+		assertEquals(Action.SUCCESS, result);
+
+		PageSettingsAction action = (PageSettingsAction) this.getAction();
+		Map<String, String> params = action.getSystemParams();
+		assertTrue(params.size() >= 6);
+		assertEquals("homepage", params.get(SystemConstants.CONFIG_PARAM_HOMEPAGE_PAGE_CODE));
+	}
+
+	public void testUpdateConfigParams_1() throws Throwable {
+        try {
+            this.setUserOnSession("admin");
+            this.initAction("/do/Page", "updateSystemParams");
+            this.addParameter(PageActionSettingsAspect.PARAM_ROBOT_CONTENT_CODE, "Test content");
+            String result = this.executeAction();
+            assertEquals(Action.SUCCESS, result);
+            assertTrue(this.storageManager.exists(PageActionSettingsAspect.ROBOT_FILENAME, false));
+            assertNull(this.configManager.getParam(JpseoSystemConstants.ROBOT_ALTERNATIVE_PATH_PARAM_NAME));
+            
+            this.initAction("/do/Page", "updateSystemParams");
+            this.addParameter(PageActionSettingsAspect.PARAM_ROBOT_CONTENT_CODE, "");
+            result = this.executeAction();
+            assertEquals(Action.SUCCESS, result);
+            assertFalse(this.storageManager.exists(PageActionSettingsAspect.ROBOT_FILENAME, false));
+            assertNull(this.configManager.getParam(JpseoSystemConstants.ROBOT_ALTERNATIVE_PATH_PARAM_NAME));
+        } catch (Exception e) {
+            this.storageManager.deleteFile(PageActionSettingsAspect.ROBOT_FILENAME, false);
+            throw e;
+        }
+	}
+
+	public void testUpdateConfigParams_2() throws Throwable {
+        String path = "target/test/robot_test.txt";
+        try {
+            this.setUserOnSession("admin");
+            this.initAction("/do/Page", "updateSystemParams");
+            this.addParameter(PageActionSettingsAspect.PARAM_ROBOT_CONTENT_CODE, "test content");
+            this.addParameter(PageActionSettingsAspect.PARAM_ROBOT_ALTERNATIVE_PATH_CODE, path);
+            String result = this.executeAction();
+            assertEquals(Action.SUCCESS, result);
+            File added = new File(path);
+            assertTrue(added.exists());
+            assertNotNull(this.configManager.getParam(JpseoSystemConstants.ROBOT_ALTERNATIVE_PATH_PARAM_NAME));
+            
+            this.initAction("/do/Page", "updateSystemParams");
+            this.addParameter(PageActionSettingsAspect.PARAM_ROBOT_CONTENT_CODE, "");
+            this.addParameter(PageActionSettingsAspect.PARAM_ROBOT_ALTERNATIVE_PATH_CODE, path);
+            result = this.executeAction();
+            assertEquals(Action.SUCCESS, result);
+            added = new File(path);
+            assertFalse(added.exists());
+            assertNotNull(this.configManager.getParam(JpseoSystemConstants.ROBOT_ALTERNATIVE_PATH_PARAM_NAME));
+        } catch (Exception e) {
+            File added = new File(path);
+            if (added.exists()) {
+                added.delete();
+            } 
+            throw e;
+        }
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		this.configManager.updateConfigItem(SystemConstants.CONFIG_ITEM_PARAMS, this.oldConfigParam);
+		super.tearDown();
+	}
+
+	private void init() {
+		this.configManager = (ConfigInterface) this.getService(SystemConstants.BASE_CONFIG_MANAGER);
+		this.storageManager = this.getApplicationContext().getBean(SystemConstants.STORAGE_MANAGER, IStorageManager.class);
+		this.oldConfigParam = this.configManager.getConfigItem(SystemConstants.CONFIG_ITEM_PARAMS);
+	}
+
+}
