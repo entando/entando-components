7a244dcfaa586b687d5736a185f55b2b536970f0
Ken Spokas <k.spokas@entando.com>
2018-07-16 00:04:52 -0400

    Small change to allow task detail to popup when using pam7

--- plugins/entando-plugin-jpkiebpm/src/test/java/org/entando/entando/plugins/jpkiebpm/aps/system/services/kie/model/pamSeven/TestPamQueryFormResult.java
+++ plugins/entando-plugin-jpkiebpm/src/test/java/org/entando/entando/plugins/jpkiebpm/aps/system/services/kie/model/pamSeven/TestPamQueryFormResult.java
@@ -3,15 +3,63 @@ package org.entando.entando.plugins.jpkiebpm.aps.system.services.kie.model.pamSe
 
 import junit.framework.TestCase;
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 import org.entando.entando.plugins.jpkiebpm.aps.system.services.kie.KieVersionTransformer;
 import org.entando.entando.plugins.jpkiebpm.aps.system.services.kie.model.KieProcessFormQueryResult;
+import org.entando.entando.plugins.jpkiebpm.aps.system.services.kie.model.KieTaskDetail;
 import org.entando.entando.plugins.jprestapi.aps.core.helper.JAXBHelper;
 import org.junit.Test;
 
 import java.io.File;
 import java.util.List;
 
+import javax.xml.transform.stream.StreamSource;
+
 public class TestPamQueryFormResult extends TestCase {
+	private final static transient Logger logger = Logger.getLogger(TestPamQueryFormResult.class);
+	
+	@Test
+	public void testTaskDetailMarshal() throws Throwable{
+		String response = "{\n" + 
+				"  \"task-id\" : 1,\n" + 
+				"  \"task-priority\" : 0,\n" + 
+				"  \"task-name\" : \"Manually Appraise Borrower\",\n" + 
+				"  \"task-subject\" : \"\",\n" + 
+				"  \"task-description\" : \"\",\n" + 
+				"  \"task-type\" : null,\n" + 
+				"  \"task-form\" : \"Appraisal\",\n" + 
+				"  \"task-status\" : \"Ready\",\n" + 
+				"  \"task-actual-owner\" : \"\",\n" + 
+				"  \"task-created-by\" : \"\",\n" + 
+				"  \"task-created-on\" : {\n" + 
+				"  \"java.util.Date\" : 1531691851738\n" + 
+				"},\n" + 
+				"  \"task-activation-time\" : {\n" + 
+				"  \"java.util.Date\" : 1531691851738\n" + 
+				"},\n" + 
+				"  \"task-expiration-time\" : null,\n" + 
+				"  \"task-skippable\" : true,\n" + 
+				"  \"task-workitem-id\" : 1,\n" + 
+				"  \"task-process-instance-id\" : 1,\n" + 
+				"  \"task-parent-id\" : -1,\n" + 
+				"  \"task-process-id\" : \"com.redhat.bpms.examples.mortgage.MortgageApplication\",\n" + 
+				"  \"task-container-id\" : \"mortgage_1.0\",\n" + 
+				"  \"task-pot-owners\" : [ \"appraiser\" ],\n" + 
+				"  \"task-excl-owners\" : [ ],\n" + 
+				"  \"task-business-admins\" : [ \"Administrator\", \"Administrators\" ],\n" + 
+				"  \"task-input-data\" : null,\n" + 
+				"  \"task-output-data\" : null\n" + 
+				"}";
+		
+		StreamSource ss = new StreamSource(new java.io.StringReader(response));
+		
+		Object obj = JAXBHelper.unmarshall(ss, KieTaskDetail.class, false, true);
+		
+		logger.info("Obj:" + obj);
+		
+		assertNotNull("Expecting non-null marshalled object", obj);
+		
+	}
 
     @Test
     public void testPamSevenFormParse() throws Throwable{
