3a606abdc7cc61b4a06ddcbe73a2946975c4dfe5
Davide Minnai <d.minnai@entando.com>
2018-07-16 11:28:24 +0200

    widgets name refactor (#279)

--- ui-bundles/widgets/entando-widget-search_form_inspinia/src/main/resources/sql/bundles/entando-widget-search_form_inspinia/port_data_production.sql
+++ ui-bundles/widgets/entando-widget-search_form_inspinia/src/main/resources/sql/bundles/entando-widget-search_form_inspinia/port_data_production.sql
@@ -1,7 +1,7 @@
 INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('entando-widget-search_form_inspinia', '<?xml version="1.0" encoding="UTF-8"?>
 <properties>
-<property key="en">Search Form</property>
-<property key="it">Search Form</property>
+<property key="en">Search Form (INSPINIA)</property>
+<property key="it">Search Form (INSPINIA)</property>
 </properties>', NULL, NULL, NULL, NULL, 1, NULL);
 
 INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('entando-widget-search_form_inspinia', 'entando-widget-search_form_inspinia', NULL, NULL, '<#assign wp=JspTaglibs["/aps-core"]>
