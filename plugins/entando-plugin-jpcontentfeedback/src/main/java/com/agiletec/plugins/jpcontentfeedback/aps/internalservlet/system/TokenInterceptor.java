5e60b34ab39cc363179aee4e8ee86ac43791a4cb
joewhite101 <j.white@entando.com>
2018-09-06 10:42:27 +0200

    EN-1913 prospective struts upgrade

--- plugins/entando-plugin-jpcontentfeedback/src/main/java/com/agiletec/plugins/jpcontentfeedback/aps/internalservlet/system/TokenInterceptor.java
+++ plugins/entando-plugin-jpcontentfeedback/src/main/java/com/agiletec/plugins/jpcontentfeedback/aps/internalservlet/system/TokenInterceptor.java
@@ -22,8 +22,9 @@
 package com.agiletec.plugins.jpcontentfeedback.aps.internalservlet.system;
 
 import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.inject.Inject;
 import com.opensymphony.xwork2.interceptor.ValidationAware;
-import com.opensymphony.xwork2.util.LocalizedTextUtil;
+import org.entando.entando.apsadmin.system.resource.CustomTextProviderFactory;
 
 /**
  * This interceptor can make sure that back buttons and double clicks don't cause un-intended side affects.
@@ -39,14 +40,15 @@ import com.opensymphony.xwork2.util.LocalizedTextUtil;
  * @author D.Cherchi
  */
 public class TokenInterceptor extends org.apache.struts2.interceptor.TokenInterceptor {
-	
+	private CustomTextProviderFactory textProvider;
+
 	@Override
     protected String handleInvalidToken(ActionInvocation invocation) throws Exception {
         Object action = invocation.getAction();
-        String errorMessage = LocalizedTextUtil.findText(this.getClass(), "struts.messages.invalid.token",
+        String errorMessage = this.textProvider.getLocalizedTextProvider().findText(this.getClass(), "struts.messages.invalid.token",
                 invocation.getInvocationContext().getLocale(),
                 "The form has already been processed or no token was supplied, please try again.", new Object[0]);
-        String message = LocalizedTextUtil.findText(this.getClass(), "struts.messages.invalid.token.message",
+        String message = this.textProvider.getLocalizedTextProvider().findText(this.getClass(), "struts.messages.invalid.token.message",
                 invocation.getInvocationContext().getLocale(),
                 "Stop double-submission of forms.", new Object[0]);
         if (action instanceof ValidationAware) {
@@ -63,7 +65,13 @@ public class TokenInterceptor extends org.apache.struts2.interceptor.TokenInterc
         }
         return INVALID_TOKEN_CODE;
     }
-	
+
+	@Inject
+	public void setLocalizedTextProvider(CustomTextProviderFactory textProvider) {
+		this.textProvider = textProvider;
+	}
+
+
 	public void setTypeMessages(String typeMessages) {
 		this._typeMessages = typeMessages;
 	}
