2b116f380c8a83bffa395fdd0892bd0bca0548f4
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-09-17 11:50:10 +0200

    EN-1937 Upgrade taglibs and cxf version

--- plugins/entando-plugin-jpfacetnav/src/main/java/com/agiletec/plugins/jpfacetnav/aps/tags/FacetNodeTitleTag.java
+++ plugins/entando-plugin-jpfacetnav/src/main/java/com/agiletec/plugins/jpfacetnav/aps/tags/FacetNodeTitleTag.java
@@ -24,7 +24,6 @@ package com.agiletec.plugins.jpfacetnav.aps.tags;
 import javax.servlet.ServletRequest;
 import javax.servlet.jsp.JspException;
 
-import org.apache.taglibs.standard.tag.common.core.OutSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,103 +36,92 @@ import com.agiletec.aps.system.services.lang.Lang;
 import com.agiletec.aps.util.ApsWebApplicationUtils;
 import com.agiletec.plugins.jpfacetnav.aps.system.JpFacetNavSystemConstants;
 import com.agiletec.plugins.jpfacetnav.aps.system.services.content.widget.IFacetNavHelper;
+import org.entando.entando.aps.tags.ExtendedTagSupport;
 
 /**
- * 
+ *
  * @author E.Santoboni
  */
-public class FacetNodeTitleTag extends OutSupport {
-	
-	private static final Logger _logger = LoggerFactory.getLogger(FacetNodeTitleTag.class);
-	
-	@Override
-	public int doStartTag() throws JspException {
-		ServletRequest request =  this.pageContext.getRequest();
-		RequestContext reqCtx = (RequestContext) request.getAttribute(RequestContext.REQCTX);
-		try {
-			IFacetNavHelper facetNavHelper = (IFacetNavHelper) ApsWebApplicationUtils.getBean(JpFacetNavSystemConstants.CONTENT_FACET_NAV_HELPER, this.pageContext);
-			ITreeNodeManager facetManager =  facetNavHelper.getTreeNodeManager();
-			ITreeNode facetNode = facetManager.getNode(this.getFacetNodeCode());
-			String separator = (this.getSeparator() == null) ? " / " : this.getSeparator();
-			if (null != facetNode) {
-				Lang currentLang = (Lang) reqCtx.getExtraParam(SystemConstants.EXTRAPAR_CURRENT_LANG);
-				String title = facetNode.getTitles().getProperty(currentLang.getCode());
-				if (this.isFullTitle()) {
-					title = facetNode.getFullTitle(currentLang.getCode(), separator);
-				}
-				if (null == title || title.trim().length() == 0) {
-					ILangManager langManager = (ILangManager) ApsWebApplicationUtils.getBean(SystemConstants.LANGUAGE_MANAGER, this.pageContext);
-					Lang defaultLang = langManager.getDefaultLang();
-					title = facetNode.getTitles().getProperty(defaultLang.getCode());
-					if (this.isFullTitle()) {
-						title = facetNode.getFullTitle(defaultLang.getCode(), separator);
-					}
-				}
-				if (null == title || title.trim().length() == 0) {
-					title = this.getFacetNodeCode();
-				}
-				
-				if (this.getEscapeXml()) {
-					out(this.pageContext, this.getEscapeXml(), title);
-				} else {
-					this.pageContext.getOut().print(title);
-				}
-			} else this.pageContext.getOut().print("UNKNOWN FACET");
-			
-		} catch (Throwable t) {
-			_logger.error("error in doStartTag", t);
-			throw new JspException("error in doStartTag", t);
-		}
-		return super.doStartTag();
-	}
-	
-	@Override
-	public void release() {
-		super.release();
-		this._facetNodeCode = null;
-		this._fullTitle = false;
-		this._separator = " / ";
-	}
-	
-	public String getFacetNodeCode() {
-		return _facetNodeCode;
-	}
-	public void setFacetNodeCode(String facetNodeCode) {
-		this._facetNodeCode = facetNodeCode;
-	}
-	
-	public boolean isFullTitle() {
-		return _fullTitle;
-	}
-	public void setFullTitle(boolean fullTitle) {
-		this._fullTitle = fullTitle;
-	}
-	
-	public String getSeparator() {
-		return _separator;
-	}
-	public void setSeparator(String separator) {
-		this._separator = separator;
-	}
+public class FacetNodeTitleTag extends ExtendedTagSupport {
 
-	/**
-	 * Determina se effettuare l'escape dei caratteri speciali nella label ricavata.
-	 * @return True nel caso si debba effettuare l'escape, false in caso contrario.
-	 */
-	public boolean getEscapeXml() {
-		return super.escapeXml;
-	}
-	
-	/**
-	 * Setta se effettuare l'escape dei caratteri speciali nella label ricavata.
-	 * @param escapeXml True nel caso si debba effettuare l'escape, false in caso contrario.
-	 */
-	public void setEscapeXml(boolean escapeXml) {
-		super.escapeXml = escapeXml;
-	}
-	
-	private String _facetNodeCode;
-	private boolean _fullTitle = false;
-	private String _separator = " / ";
-	
+    private static final Logger _logger = LoggerFactory.getLogger(FacetNodeTitleTag.class);
+
+    @Override
+    public int doStartTag() throws JspException {
+        ServletRequest request = this.pageContext.getRequest();
+        RequestContext reqCtx = (RequestContext) request.getAttribute(RequestContext.REQCTX);
+        try {
+            IFacetNavHelper facetNavHelper = (IFacetNavHelper) ApsWebApplicationUtils.getBean(JpFacetNavSystemConstants.CONTENT_FACET_NAV_HELPER, this.pageContext);
+            ITreeNodeManager facetManager = facetNavHelper.getTreeNodeManager();
+            ITreeNode facetNode = facetManager.getNode(this.getFacetNodeCode());
+            String separator = (this.getSeparator() == null) ? " / " : this.getSeparator();
+            if (null != facetNode) {
+                Lang currentLang = (Lang) reqCtx.getExtraParam(SystemConstants.EXTRAPAR_CURRENT_LANG);
+                String title = facetNode.getTitles().getProperty(currentLang.getCode());
+                if (this.isFullTitle()) {
+                    title = facetNode.getFullTitle(currentLang.getCode(), separator);
+                }
+                if (null == title || title.trim().length() == 0) {
+                    ILangManager langManager = (ILangManager) ApsWebApplicationUtils.getBean(SystemConstants.LANGUAGE_MANAGER, this.pageContext);
+                    Lang defaultLang = langManager.getDefaultLang();
+                    title = facetNode.getTitles().getProperty(defaultLang.getCode());
+                    if (this.isFullTitle()) {
+                        title = facetNode.getFullTitle(defaultLang.getCode(), separator);
+                    }
+                }
+                if (null == title || title.trim().length() == 0) {
+                    title = this.getFacetNodeCode();
+                }
+                if (this.getEscapeXml()) {
+                    out(this.pageContext, this.getEscapeXml(), title);
+                } else {
+                    this.pageContext.getOut().print(title);
+                }
+            } else {
+                this.pageContext.getOut().print("UNKNOWN FACET");
+            }
+
+        } catch (Throwable t) {
+            _logger.error("error in doStartTag", t);
+            throw new JspException("error in doStartTag", t);
+        }
+        return super.doStartTag();
+    }
+
+    @Override
+    public void release() {
+        super.release();
+        this._facetNodeCode = null;
+        this._fullTitle = false;
+        this._separator = " / ";
+    }
+
+    public String getFacetNodeCode() {
+        return _facetNodeCode;
+    }
+
+    public void setFacetNodeCode(String facetNodeCode) {
+        this._facetNodeCode = facetNodeCode;
+    }
+
+    public boolean isFullTitle() {
+        return _fullTitle;
+    }
+
+    public void setFullTitle(boolean fullTitle) {
+        this._fullTitle = fullTitle;
+    }
+
+    public String getSeparator() {
+        return _separator;
+    }
+
+    public void setSeparator(String separator) {
+        this._separator = separator;
+    }
+
+    private String _facetNodeCode;
+    private boolean _fullTitle = false;
+    private String _separator = " / ";
+
 }
