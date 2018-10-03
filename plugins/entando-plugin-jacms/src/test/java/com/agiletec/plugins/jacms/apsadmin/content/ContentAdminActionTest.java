5958a6a49bd7372f97f515f867fe6acc31d995c7
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-10-03 12:49:41 +0200

    EN-1977 Improvement of ContentAdminActionTest

--- plugins/entando-plugin-jacms/src/test/java/com/agiletec/plugins/jacms/apsadmin/content/ContentAdminActionTest.java
+++ plugins/entando-plugin-jacms/src/test/java/com/agiletec/plugins/jacms/apsadmin/content/ContentAdminActionTest.java
@@ -13,17 +13,27 @@
  */
 package com.agiletec.plugins.jacms.apsadmin.content;
 
-import com.agiletec.aps.system.services.authorization.IAuthorizationManager;
+import com.agiletec.aps.system.exception.ApsSystemException;
+import com.agiletec.aps.system.services.baseconfig.ConfigInterface;
 import com.agiletec.aps.system.services.lang.ILangManager;
+import com.agiletec.apsadmin.system.BaseAction;
+import com.agiletec.plugins.jacms.aps.system.services.content.IContentManager;
+import com.agiletec.plugins.jacms.aps.system.services.resource.IResourceManager;
+import com.agiletec.plugins.jacms.aps.system.services.searchengine.ICmsSearchEngineManager;
+import com.opensymphony.xwork2.TextProvider;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 
@@ -32,53 +42,205 @@ import org.mockito.MockitoAnnotations;
  */
 public class ContentAdminActionTest {
 
+    private static final String CONFIG_PARAMETER
+            = "<Params><Param name=\"param_1\">value_1</Param><Param name=\"param_2\">value_2</Param></Params>";
+
+    private static final String CONFIG_MAPPING
+            = "<Params><Param name=\"param_1\">value_1</Param><Param name=\"param_2\">value_2</Param></Params>";
+
     @Mock
     private HttpServletRequest request;
     @Mock
     private ILangManager langManager;
     @Mock
-    private IAuthorizationManager authorizationManager;
+    private ConfigInterface configManager;
+    @Mock
+    private IResourceManager resourceManager;
+    @Mock
+    private TextProvider textProvider;
+    @Mock
+    private ICmsSearchEngineManager searchEngineManager;
+    @Mock
+    private IContentManager contentManager;
+
     @InjectMocks
     private ContentAdminAction action;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-    }
-
-    @Test
-    public void validate_1() {
-        List<String> metadataKeys = new ArrayList<>();
-        metadataKeys.add("metadata_1");
-        metadataKeys.add("metadata_2");
-        this.action.setMetadataKeys(metadataKeys);
-        this.action.setMetadataKey("metadata_3");
-        when(request.getParameter("resourceMetadata_mapping_metadata_1")).thenReturn("value1,value2,value3,value4");
-        when(request.getParameter("resourceMetadata_mapping_metadata_2")).thenReturn("Value_a");
-        this.action.validate();
-        Map<String, List<String>> mapping = this.action.getMapping();
-        Assert.assertEquals(2, mapping.size());
-        Assert.assertEquals(4, mapping.get("metadata_1").size());
-        Assert.assertEquals(1, mapping.get("metadata_2").size());
-        Assert.assertNull(mapping.get("metadata_3"));
-    }
-
-    @Test
-    public void validate_2() {
+        when(textProvider.getText(ArgumentMatchers.anyString(), ArgumentMatchers.anyList())).thenReturn(ArgumentMatchers.anyString());
         List<String> metadataKeys = new ArrayList<>();
         metadataKeys.add("metadata_A");
         metadataKeys.add("metadata_B");
         this.action.setMetadataKeys(metadataKeys);
-        this.action.setMetadataKey("metadata_B");
-        when(request.getParameter("resourceMetadata_mapping_metadata_A")).thenReturn("value1,value2");
+        when(request.getParameter("resourceMetadata_mapping_metadata_A")).thenReturn("value1,value2,value3");
         when(request.getParameter("resourceMetadata_mapping_metadata_B")).thenReturn("Value_a");
+    }
+
+    @Test
+    public void validate_1() {
+        this.action.setMetadataKey("metadata_C");
         this.action.validate();
         Map<String, List<String>> mapping = this.action.getMapping();
         Assert.assertEquals(2, mapping.size());
-        Assert.assertEquals(2, mapping.get("metadata_A").size());
+        Assert.assertEquals(3, mapping.get("metadata_A").size());
+        Assert.assertEquals(1, mapping.get("metadata_B").size());
+        Assert.assertNull(mapping.get("metadata_C"));
+    }
+
+    @Test
+    public void validate_2() {
+        this.action.setMetadataKey("metadata_B");
+        this.action.validate();
+        Map<String, List<String>> mapping = this.action.getMapping();
+        Assert.assertEquals(2, mapping.size());
+        Assert.assertEquals(3, mapping.get("metadata_A").size());
         Assert.assertEquals(1, mapping.get("metadata_B").size());
         Assert.assertTrue(action.getFieldErrors().size() > 0);
         Assert.assertEquals(1, action.getFieldErrors().get("metadataKey").size());
+    }
+
+    @Test
+    public void addMetadata_1() {
+        this.action.setMetadataKey("metadata_A");
+        this.action.addMetadata();
+        Map<String, List<String>> mapping = this.action.getMapping();
+        Assert.assertEquals(2, mapping.size());
+        Assert.assertEquals(3, mapping.get("metadata_A").size());
+        Assert.assertEquals(1, mapping.get("metadata_B").size());
+        Assert.assertEquals(0, action.getFieldErrors().size());
+    }
+
+    @Test
+    public void addMetadata_2() {
+        this.action.setMetadataKey("metadata_C");
+        this.action.addMetadata();
+        Map<String, List<String>> mapping = this.action.getMapping();
+        Assert.assertEquals(3, mapping.size());
+        Assert.assertEquals(3, mapping.get("metadata_A").size());
+        Assert.assertEquals(1, mapping.get("metadata_B").size());
+        Assert.assertEquals(0, mapping.get("metadata_C").size());
+        Assert.assertEquals(0, action.getFieldErrors().size());
+    }
+
+    @Test
+    public void addMetadata_3() {
+        this.action.setMetadataKey("metadata_C");
+        this.action.setMetadataMapping("Value_X,ValueY,Value_Z,Value_K,Value_J");
+        this.action.addMetadata();
+        Map<String, List<String>> mapping = this.action.getMapping();
+        Assert.assertEquals(3, mapping.size());
+        Assert.assertEquals(3, mapping.get("metadata_A").size());
+        Assert.assertEquals(1, mapping.get("metadata_B").size());
+        Assert.assertEquals(5, mapping.get("metadata_C").size());
+        Assert.assertEquals(0, action.getFieldErrors().size());
+    }
+
+    @Test
+    public void removeMetadata_1() {
+        when(request.getParameter("resourceMetadata_mapping_metadata_A")).thenReturn("value1,value2,value3,newValue");
+        this.action.setMetadataKey("metadata_C");
+        this.action.removeMetadata();
+        Map<String, List<String>> mapping = this.action.getMapping();
+        Assert.assertEquals(2, mapping.size());
+        Assert.assertEquals(4, mapping.get("metadata_A").size());
+        Assert.assertEquals(1, mapping.get("metadata_B").size());
+    }
+
+    @Test
+    public void removeMetadata_2() {
+        this.action.setMetadataKey("metadata_A");
+        this.action.removeMetadata();
+        Map<String, List<String>> mapping = this.action.getMapping();
+        Assert.assertEquals(1, mapping.size());
+        Assert.assertEquals(1, mapping.get("metadata_B").size());
+    }
+
+    @Test
+    public void configSystemParams_1() {
+        when(configManager.getConfigItem(Mockito.anyString())).thenThrow(RuntimeException.class);
+        String result = action.configSystemParams();
+        Assert.assertEquals(BaseAction.FAILURE, result);
+        Mockito.verify(resourceManager, Mockito.times(0)).getMetadataMapping();
+    }
+
+    @Test
+    public void configSystemParams_2() {
+        when(configManager.getConfigItem(Mockito.anyString())).thenReturn(CONFIG_PARAMETER);
+        when(resourceManager.getMetadataMapping()).thenThrow(RuntimeException.class);
+        String result = action.configSystemParams();
+        Assert.assertEquals(BaseAction.FAILURE, result);
+    }
+
+    @Test
+    public void configSystemParams_3() {
+        when(configManager.getConfigItem(Mockito.anyString())).thenReturn(CONFIG_PARAMETER);
+        Map<String, List<String>> mapping = new HashMap<>();
+        mapping.put("param_1", new ArrayList<>());
+        when(resourceManager.getMetadataMapping()).thenReturn(mapping);
+        String result = action.configSystemParams();
+        Assert.assertEquals(BaseAction.SUCCESS, result);
+        Map<String, List<String>> extractedMapping = this.action.getMapping();
+        Assert.assertEquals(1, extractedMapping.size());
+        Assert.assertEquals(0, extractedMapping.get("param_1").size());
+    }
+
+    @Test
+    public void updateSystemParams_1() throws Exception {
+        when(configManager.getConfigItem(ArgumentMatchers.anyString())).thenReturn(CONFIG_PARAMETER);
+        Enumeration mockedEnumerator = Mockito.mock(Enumeration.class);
+        when(mockedEnumerator.hasMoreElements()).thenReturn(false);
+        when(request.getParameterNames()).thenReturn(mockedEnumerator);
+        Mockito.doThrow(ApsSystemException.class).when(configManager).updateConfigItem(Mockito.anyString(), Mockito.anyString());
+        String result = action.updateSystemParams();
+        Assert.assertEquals(BaseAction.FAILURE, result);
+        Mockito.verify(resourceManager, Mockito.times(0)).updateMetadataMapping(Mockito.any(Map.class));
+    }
+
+    @Test
+    public void updateSystemParams_2() throws Exception {
+        when(configManager.getConfigItem(ArgumentMatchers.anyString())).thenReturn(CONFIG_PARAMETER);
+        Enumeration mockedEnumerator = Mockito.mock(Enumeration.class);
+        when(mockedEnumerator.hasMoreElements()).thenReturn(false);
+        when(request.getParameterNames()).thenReturn(mockedEnumerator);
+        //Mockito.doThrow(ApsSystemException.class).when(configManager).updateConfigItem(Mockito.anyString(), Mockito.anyString());
+        String result = action.updateSystemParams();
+        Assert.assertEquals(BaseAction.SUCCESS, result);
+        Mockito.verify(resourceManager, Mockito.times(1)).updateMetadataMapping(Mockito.any(Map.class));
+        Mockito.verify(configManager, Mockito.times(1)).updateConfigItem(Mockito.anyString(), Mockito.anyString());
+    }
+
+    @Test
+    public void reloadContentsIndex_1() throws Exception {
+        when(configManager.getConfigItem(ArgumentMatchers.anyString())).thenReturn(CONFIG_PARAMETER);
+        when(searchEngineManager.startReloadContentsReferences()).thenThrow(ApsSystemException.class);
+        String result = action.reloadContentsIndex();
+        Assert.assertEquals(BaseAction.FAILURE, result);
+    }
+
+    @Test
+    public void reloadContentsIndex_2() throws Exception {
+        when(configManager.getConfigItem(ArgumentMatchers.anyString())).thenReturn(CONFIG_PARAMETER);
+        String result = action.reloadContentsIndex();
+        Assert.assertEquals(BaseAction.SUCCESS, result);
+        Mockito.verify(searchEngineManager, Mockito.times(1)).startReloadContentsReferences();
+    }
+
+    @Test
+    public void reloadContentsReference_1() throws Exception {
+        when(configManager.getConfigItem(ArgumentMatchers.anyString())).thenReturn(CONFIG_PARAMETER);
+        when(contentManager.reloadEntitiesReferences(null)).thenThrow(RuntimeException.class);
+        String result = action.reloadContentsReference();
+        Assert.assertEquals(BaseAction.FAILURE, result);
+    }
+
+    @Test
+    public void reloadContentsReference_2() throws Exception {
+        when(configManager.getConfigItem(ArgumentMatchers.anyString())).thenReturn(CONFIG_PARAMETER);
+        String result = action.reloadContentsReference();
+        Assert.assertEquals(BaseAction.SUCCESS, result);
+        Mockito.verify(contentManager, Mockito.times(1)).reloadEntitiesReferences(null);
     }
 
 }
