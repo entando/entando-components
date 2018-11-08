4a717de79219ca15e59e8c78fc9d3050ef68080b
davide.minnai@entando.com <d.minnai@entando.com>
2018-11-08 10:42:55 +0100

    sql updated on specs

--- ui-bundles/themes/entando-page-inspinia_BPM/src/main/resources/sql/bundles/entando-page-inspinia_BPM/port_data_production.sql
+++ ui-bundles/themes/entando-page-inspinia_BPM/src/main/resources/sql/bundles/entando-page-inspinia_BPM/port_data_production.sql
@@ -1,104 +1,73 @@
 INSERT INTO pagemodels (code, descr, frames, plugincode, templategui) VALUES ('entando-page-inspinia_BPM', 'Inspinia - BPM Case Management widgets', '<frames>
-    <frame pos="0">
-        <descr>Sidebar 1</descr>
-        <sketch x1="0" y1="0" x2="1" y2="0" />
-    </frame>
-    <frame pos="1">
-        <descr>Top Bar 1</descr>
-        <sketch x1="2" y1="0" x2="4" y2="0" />
-    </frame>
-    <frame pos="2">
-        <descr>Top Bar 2</descr>
-        <sketch x1="5" y1="0" x2="7" y2="0" />
-    </frame>
-    <frame pos="3">
-        <descr>Top Bar 3</descr>
-        <sketch x1="8" y1="0" x2="9" y2="0" />
-    </frame>
-    <frame pos="4">
-        <descr>Top Bar 4</descr>
-        <sketch x1="10" y1="0" x2="11" y2="0" />
-    </frame>
-    <frame pos="5">
-        <descr>Left</descr>
-        <sketch x1="2" y1="1" x2="4" y2="1" />
-    </frame>
-    <frame pos="6">
-        <descr>Center</descr>
-        <sketch x1="5" y1="1" x2="7" y2="1" />
-    </frame>
-    <frame pos="7">
-        <descr>Right</descr>
-        <sketch x1="8" y1="1" x2="11" y2="1" />
-    </frame>
-    <frame pos="8">
-        <descr>Sidebar 2</descr>
-        <sketch x1="0" y1="1" x2="1" y2="1" />
-    </frame>
-    <frame pos="9">
-        <descr>Full 1</descr>
-        <sketch x1="2" y1="2" x2="11" y2="2" />
-    </frame>
-    <frame pos="10">
-        <descr>Sidebar 3</descr>
-        <sketch x1="0" y1="2" x2="1" y2="2" />
-    </frame>
-    <frame pos="11" main="true">
-        <descr>full 2</descr>
-        <sketch x1="2" y1="3" x2="11" y2="3" />
-    </frame>
-    <frame pos="12">
-        <descr>Sidebar 4</descr>
-        <sketch x1="0" y1="3" x2="1" y2="3" />
-    </frame>
-    <frame pos="13">
-        <descr>Content left</descr>
-        <sketch x1="2" y1="4" x2="6" y2="4" />
-    </frame>
-    <frame pos="14">
-        <descr>Content right</descr>
-        <sketch x1="7" y1="4" x2="11" y2="4" />
-    </frame>
-    <frame pos="15">
-        <descr>Sidebar 5</descr>
-        <sketch x1="0" y1="4" x2="1" y2="4" />
-    </frame>
-    <frame pos="16">
-        <descr>Full 3</descr>
-        <sketch x1="2" y1="5" x2="11" y2="5" />
-    </frame>
-    <frame pos="17">
-        <descr>Sidebar 5</descr>
-        <sketch x1="0" y1="5" x2="1" y2="5" />
-    </frame>
-    <frame pos="18">
-        <descr>Left</descr>
-        <sketch x1="0" y1="6" x2="3" y2="6" />
-    </frame>
-    <frame pos="19">
-        <descr>Center</descr>
-        <sketch x1="4" y1="6" x2="7" y2="6" />
-    </frame>
-    <frame pos="20">
-        <descr>Right</descr>
-        <sketch x1="8" y1="6" x2="11" y2="6" />
-    </frame>
-    <frame pos="21">
-        <descr>Left</descr>
-        <sketch x1="0" y1="7" x2="3" y2="7" />
-    </frame>
-    <frame pos="22">
-        <descr>Center</descr>
-        <sketch x1="4" y1="7" x2="7" y2="7" />
-    </frame>
-    <frame pos="23">
-        <descr>Right</descr>
-        <sketch x1="8" y1="7" x2="11" y2="7" />
-    </frame>
-    <frame pos="24">
-        <descr>Footer</descr>
-        <sketch x1="0" y1="8" x2="11" y2="8" />
-    </frame>
+	<frame pos="0">
+		<descr>Header sample</descr>
+		<sketch x1="0" y1="0" x2="11" y2="0" />
+		<defaultWidget code="bpm-case-instance-selector">
+			<properties>
+				<property key="channel">1</property>
+				<property key="frontEndCaseData">{"container-id":"itorders_1.0.0-SNAPSHOT","knowledge-source-id":"1"}</property>
+			</properties>
+		</defaultWidget>
+	</frame>
+	<frame pos="1">
+		<descr>Case details</descr>
+		<sketch x1="0" y1="1" x2="11" y2="1" />
+		<defaultWidget code="bpm-case-details">
+			<properties>
+				<property key="channel">1</property>
+			</properties>
+		</defaultWidget>
+	</frame>
+	<frame pos="2">
+		<descr>Progress Bar basic with label on center</descr>
+		<sketch x1="0" y1="2" x2="11" y2="2" />
+		<defaultWidget code="bpm-case-progress-status">
+			<properties>
+				<property key="channel">1</property>
+				<property key="frontEndMilestonesData">{"container-id":"itorders_1.0.0-SNAPSHOT","name":"Order for IT hardware","knowledge-source-id":"1","case-id-prefix":"IT","stages":[],"id":"itorders.orderhardware","milestones":[{"milestone-id":"_DCD97847-6E3C-4C5E-9EE3-221C04BE42ED","milestone-name":"Milestone 1: Order placed","milestone-mandatory":false,"visible":true,"percentage":20},{"milestone-id":"_343B90CD-AA19-4894-B63C-3CE1906E6FD1","milestone-name":"Milestone 2: Order shipped","milestone-mandatory":false,"visible":true,"percentage":20},{"milestone-id":"_52AFA23F-C087-4519-B8F2-BABCC31D68A6","milestone-name":"Milestone 3: Delivered to customer","milestone-mandatory":false,"visible":true,"percentage":20},{"milestone-id":"_483CF785-96DD-40C1-9148-4CFAFAE5778A","milestone-name":"Hardware spec ready","milestone-mandatory":false,"visible":true,"percentage":20},{"milestone-id":"_79953D58-25DB-4FD6-94A0-DFC6EA2D0339","milestone-name":"Manager decision","milestone-mandatory":false,"visible":true,"percentage":20}],"ui":{"progress-bar-type":"stacked","additionalSettings":["show-milestones","show-number-of-tasks"]},"version":"1.0"}</property>
+			</properties>
+		</defaultWidget>
+	</frame>
+	<frame pos="3">
+		<descr>Rules</descr>
+		<sketch x1="0" y1="3" x2="5" y2="6" />
+		<defaultWidget code="bpm-case-roles">
+			<properties>
+				<property key="channel">1</property>
+			</properties>
+		</defaultWidget>
+	</frame>
+	<frame pos="4">
+		<descr>Comments</descr>
+		<sketch x1="6" y1="3" x2="11" y2="6" />
+		<defaultWidget code="bpm-case-comments">
+			<properties>
+				<property key="channel">1</property>
+			</properties>
+		</defaultWidget>
+	</frame>
+	<frame pos="5">
+		<descr>Task Tables</descr>
+		<sketch x1="0" y1="7" x2="11" y2="10" />
+	</frame>
+	<frame pos="6">
+		<descr>Chart</descr>
+		<sketch x1="0" y1="11" x2="5" y2="14" />
+		<defaultWidget code="bpm-case-chart">
+			<properties>
+				<property key="channel">1</property>
+			</properties>
+		</defaultWidget>
+	</frame>
+	<frame pos="7">
+		<descr>Process Diagram</descr>
+		<sketch x1="6" y1="11" x2="11" y2="14" />
+		<defaultWidget code="bpm-case-file">
+			<properties>
+				<property key="channel">1</property>
+			</properties>
+		</defaultWidget>
+	</frame>
 </frames>', NULL, '<#assign wp=JspTaglibs["/aps-core"]>
 <#assign c=JspTaglibs["http://java.sun.com/jsp/jstl/core"]>
 <!DOCTYPE html>
@@ -113,148 +82,91 @@ INSERT INTO pagemodels (code, descr, frames, plugincode, templategui) VALUES ('e
         <meta name="author" content="" />
         <link rel="icon" href="<@wp.info key="systemParam" paramName="applicationBaseURL" />
               favicon.png" type="image/png" />
-              <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-              <!--[if lt IE 9]>
-              <script src="<@wp.resourceURL />static/js/entando-misc-html5-essentials/html5shiv.js"></script>
-              <![endif]-->
+          
               <@c.import url="/WEB-INF/aps/jsp/models/inc/content_inline_editing.jsp" />
               <@c.import url="/WEB-INF/aps/jsp/models/inc/header-inclusions.jsp" />
+
+
+        <style type="text/css">
+            .row {
+                padding-top:20px;
+                padding-bottom:10px;
+                border-bottom:2px solid #e7eaec;
+                margin-right: 0 !important;
+                margin-left: 0 !important;
+            }
+        </style>
     </head>
     <body class="pace-done">
-        <div class="pace  pace-inactive">
+        <div class="pace pace-inactive">
             <div class="pace-progress" data-progress-text="100%" data-progress="99" style="transform: translate3d(100%, 0px, 0px);">
                 <div class="pace-progress-inner"></div>
             </div>
             <div class="pace-activity"></div>
         </div>
         <div id="wrapper">
-            <nav class="navbar-default navbar-static-side" role="navigation">
-                <div class="sidebar-collapse">
-                    <ul class="nav metismenu" id="side-menu">
-                        <li class="nav-header">
-                            <div class="dropdown profile-element">
-                                <#if (accountExpired?? && accountExpired == true) || (wrongAccountCredential?? && wrongAccountCredential == true)>open</#if>
-                                <#if (Session.currentUser != "guest")>
-                                <span>
-                                    <img alt="image" class="" src="<@wp.imgURL />entando-logo.png">
-                                </span>
-                                <#else>
-                                <span>
-                                    <img alt="image" class="" src="<@wp.imgURL />entando-logo-1.png">
-                                </span>
-                                </#if>
-                                <br>
-                                <@wp.show frame=0 />
-                            </div>
-                            <div class="logo-element">
-                                E
-                            </div>
-                        </li>
-                        <@wp.show frame=8 />
-                        <@wp.show frame=10 />
-                        <@wp.show frame=12 />
-                        <@wp.show frame=15 />
-                        <@wp.show frame=17 />
-                    </ul>
-                </div>
-            </nav>
             <div id="page-wrapper" class="gray-bg dashbard-1">
-                <div class="row border-bottom">
-                    <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
-                        <div class="navbar-header">
-                            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#">
-                                <i class="fa fa-bars"></i>
-                            </a>
+                <div class="row white-bg">
+                    <div class="col-md-3">&nbsp;</div>
+                    <div class="col-md-6">
+                        <div class="white-bg">
+                            <@wp.show frame=0 />
                         </div>
-                        <ul class="nav navbar-top-links navbar-right">
-                            <li class="dropdown">
-                                <@wp.show frame=1 />
-                                <@wp.show frame=2 />
-                                <@wp.show frame=3 />
-                                <@wp.show frame=4 />
-                        </ul>
-                    </nav>
+                    </div>
+                    <div class="col-md-3">&nbsp;</div>
                 </div>
-                <div class="row white-bg" style="padding-top:20px; padding-bottom:10px; border-bottom:2px solid #e7eaec;">
-                    <div class="col-md-4">
+
+                <div class="row white-bg">
+                    <div class="col-md-12">
+                        <div class="white-bg">
+                            <@wp.show frame=1 />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row white-bg">
+                    <div class="col-md-12">
+                        <div class="white-bg">
+                            <@wp.show frame=2 />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row white-bg">
+                    <div class="col-md-6">
+                        <div class="white-bg">
+                            <@wp.show frame=3 />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="white-bg">
+                            <@wp.show frame=4 />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row white-bg">
+                    <div class="col-md-12">
                         <div class="white-bg">
                             <@wp.show frame=5 />
                         </div>
                     </div>
-                    <div class="col-md-4">
+                </div>
+
+                <div class="row white-bg">
+                    <div class="col-md-6">
                         <div class="white-bg">
                             <@wp.show frame=6 />
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-6">
                         <div class="white-bg">
                             <@wp.show frame=7 />
                         </div>
                     </div>
                 </div>
-                <div class="row white-bg" style="padding-top:20px; padding-bottom:10px; border-bottom:2px solid #e7eaec;">
-                    <div class="col-md-12">
-                        <div class="white-bg">
-                            <@wp.show frame=9 />
-                        </div>
-                    </div>
-                </div>
-                <div class="row white-bg" style="padding-top:20px; padding-bottom:10px; border-bottom:2px solid #e7eaec;">
-                    <div class="col-md-12">
-                        <div class="white-bg">
-                            <@wp.show frame=11 />
-                        </div>
-                    </div>
 
-                </div>
-                <div class="row white-bg" style="padding-top:20px; padding-bottom:10px; border-bottom:2px solid #e7eaec;">
-                    <div class="col-md-6">
-                        <div class="white-bg">
-                            <@wp.show frame=13 />
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="white-bg">
-                            <@wp.show frame=14 />
-                        </div>
-                    </div>
-                </div>
-                <div class="row white-bg" style="padding-top:20px; padding-bottom:10px; border-bottom:2px solid #e7eaec;">
-                    <@wp.show frame=16 />
-                </div>
-                <div class="row">
-                    <div class="wrapper wrapper-content">
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <@wp.show frame=18 />
-                            </div>
-                            <div class="col-lg-4">
-                                <@wp.show  frame=19/>
-                            </div>
-                            <div class="col-lg-4">
-                                <@wp.show  frame=20 />
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <@wp.show frame=21 />
-                            </div>
-                            <div class="col-lg-4">
-                                <@wp.show  frame=22/>
-                            </div>
-                            <div class="col-lg-4">
-                                <@wp.show  frame=23 />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="footer">
-                        <@wp.show frame=24 />
-                    </div>
-                </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>');
