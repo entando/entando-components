38cb6f8e8cb2a0d6e32f35a3a6ed52c5edb9a0d6
Sonia Zorba <s.zorba@entando.com>
2018-11-23 09:59:05 +0100

    EN-2205 Used int instead of boolean for RDBMS compatibility

--- plugins/entando-plugin-jpkiebpm/src/main/java/org/entando/entando/plugins/jpkiebpm/aps/system/init/servdb/KieFormOverride.java
+++ plugins/entando-plugin-jpkiebpm/src/main/java/org/entando/entando/plugins/jpkiebpm/aps/system/init/servdb/KieFormOverride.java
@@ -1,4 +1,5 @@
 package org.entando.entando.plugins.jpkiebpm.aps.system.init.servdb;
+
 import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
@@ -7,57 +8,58 @@ import java.util.Date;
 @DatabaseTable(tableName = KieFormOverride.TABLE_NAME)
 public class KieFormOverride {
 
-	public KieFormOverride() {}
+    public static final String TABLE_NAME = "jpkiebpm_kieformoverride";
 
-	@DatabaseField(columnName = "id",
-		dataType = DataType.INTEGER,
-		 canBeNull=false, id = true)
-	private int _id;
-        
-	@DatabaseField(columnName = "widgetinfoid",
-		dataType = DataType.INTEGER,
-		 canBeNull=false)
-	private int _widgetInfoId;
+    public KieFormOverride() {
+    }
 
-	@DatabaseField(columnName = "date",
-			dataType = DataType.DATE,
-		 canBeNull=false)
-	private Date _date;
+    @DatabaseField(columnName = "id",
+            dataType = DataType.INTEGER,
+            canBeNull = false, id = true)
+    private int id;
 
-	@DatabaseField(columnName = "field",
-		dataType = DataType.STRING,
-		width=255,  canBeNull=false)
-	private String _field;
+    @DatabaseField(columnName = "widgetinfoid",
+            dataType = DataType.INTEGER,
+            canBeNull = false)
+    private int widgetInfoId;
 
-	@DatabaseField(columnName = "containerid",
-		dataType = DataType.LONG_STRING,
-		 canBeNull=false)
-	private String _containerId;
+    @DatabaseField(columnName = "date",
+            dataType = DataType.DATE,
+            canBeNull = false)
+    private Date date;
 
-	@DatabaseField(columnName = "processid",
-		dataType = DataType.LONG_STRING,
-		 canBeNull=false)
-	private String _processId;
+    @DatabaseField(columnName = "field",
+            dataType = DataType.STRING,
+            width = 255, canBeNull = false)
+    private String field;
 
-	@DatabaseField(columnName = "sourceid",
-		dataType = DataType.LONG_STRING,
-		 canBeNull=false)
-	private String _sourceid;
+    @DatabaseField(columnName = "containerid",
+            dataType = DataType.LONG_STRING,
+            canBeNull = false)
+    private String containerId;
 
-	@DatabaseField(columnName = "override",
-		dataType = DataType.LONG_STRING,
-		 canBeNull=false)
-	private String _override;
+    @DatabaseField(columnName = "processid",
+            dataType = DataType.LONG_STRING,
+            canBeNull = false)
+    private String processId;
 
-	@DatabaseField(columnName = "active",
-		dataType = DataType.BOOLEAN,
-		 canBeNull=false)
-	private boolean _active;
+    @DatabaseField(columnName = "sourceid",
+            dataType = DataType.LONG_STRING,
+            canBeNull = false)
+    private String sourceid;
 
-	@DatabaseField(columnName = "online",
-		dataType = DataType.BOOLEAN,
-		 canBeNull=false)
-	private boolean _online;
+    @DatabaseField(columnName = "override",
+            dataType = DataType.LONG_STRING,
+            canBeNull = false)
+    private String override;
 
-	public static final String TABLE_NAME = "jpkiebpm_kieformoverride";
+    @DatabaseField(columnName = "active",
+            dataType = DataType.INTEGER,
+            canBeNull = false)
+    private int active;
+
+    @DatabaseField(columnName = "online",
+            dataType = DataType.INTEGER,
+            canBeNull = false)
+    private int online;
 }
