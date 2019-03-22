9b586fc5cb6e7df903b55f034b60645a26f8a20c
daniele.cherchi@realt.it <daniele.cherchi@realt.it>
2019-03-22 17:33:36 +0100

    spostamento plugin connectors

--- plugins/entando-plugin-dashboard-kaa-connector/src/test/java/org/entando/entando/plugins/jpkaaconnector/aps/iot/TestUtils.java
+++
@@ -1,8 +0,0 @@
-package org.entando.entando.plugins.jpkaaconnector.aps.iot;
-
-public class TestUtils {
-  public static final String KAA_SERVER_TEST_ADDRESS = "http://192.168.137.123:8080";
-  public static final String KAA_APPLICATION_TOKEN = "78851176641276848009";
-  public static final String KAA_CTL_SCHEMA_ID = "32768";
-  public static final String KAA_LOGGER_ID = "65328";
-}
--- plugins/entando-plugin-dashboard-sitewhere-connector/src/test/java/org/entando/entando/plugins/jpsitewhereconnector/aps/iot/TestUtils.java
+++
@@ -1,8 +0,0 @@
-package org.entando.entando.plugins.jpsitewhereconnector.aps.iot;
-
-public class TestUtils {
-  public static final String SITEWHERE_SERVER_TEST_ADDRESS = "http://192.168.99.100:8100";
-  public static final String SITEWHERE_HARDWARE_ID = "dispositivoAndroid";
-  public static final String SITEWHERE_TENANT_TOKEN = "sitewhere1234567890";
-  public static final String SITEWHERE_ASSIGNMENT_ID = "ba3af36a-06fb-4464-853c-665679ebee77";
-}
