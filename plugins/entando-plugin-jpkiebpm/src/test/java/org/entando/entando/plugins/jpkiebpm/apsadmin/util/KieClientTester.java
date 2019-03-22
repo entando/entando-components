17f2da6386de53b51d15968bb020daf0de925caa
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2019-03-22 17:47:46 +0100

    EN-2422 change strategy of logger strategy

--- plugins/entando-plugin-jpkiebpm/src/test/java/org/entando/entando/plugins/jpkiebpm/apsadmin/util/KieClientTester.java
+++ plugins/entando-plugin-jpkiebpm/src/test/java/org/entando/entando/plugins/jpkiebpm/apsadmin/util/KieClientTester.java
@@ -2,7 +2,8 @@ package org.entando.entando.plugins.jpkiebpm.apsadmin.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redhat.bpms.examples.mortgage.*;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.Test;
 import org.kie.server.api.model.instance.TaskSummary;
 import org.kie.server.client.KieServicesClient;
@@ -14,12 +15,12 @@ import javax.xml.bind.Marshaller;
 import java.util.*;
 
 public class KieClientTester {
-	private static final transient Logger logger = Logger.getLogger(KieClientTester.class);
+
+    private final static transient Logger logger = LoggerFactory.getLogger(KieClientTester.class);
 
     //SEE
     //https://github.com/mswiderski/jbpm-examples/blob/master/kie-server-test/src/main/java/org/jbpm/test/kieserver/KieExecutionServerClientTest.java
     // jBPM Process and Project constants
-
     private final String USERNAME = "testBroker";
 
     private final String PASSWORD = "broker1!";
@@ -32,7 +33,7 @@ public class KieClientTester {
 
     @Test
     public void run() {
-        KieServicesConfiguration config =  KieServicesFactory.newRestConfiguration(SERVER_URL, USERNAME, PASSWORD);
+        KieServicesConfiguration config = KieServicesFactory.newRestConfiguration(SERVER_URL, USERNAME, PASSWORD);
 
         Set<Class<?>> jaxbClasses = new HashSet<Class<?>>();
         jaxbClasses.add(Application.class);
@@ -45,66 +46,60 @@ public class KieClientTester {
         List<TaskSummary> tasks = taskServicesClient.findTasksAssignedAsPotentialOwner(USERNAME, 0, 10);
 
         for (TaskSummary taskSummary : tasks) {
-	    		logger.info("task: " + String.valueOf(taskSummary));
-	    }
-        
-        
-//        taskServicesClient.claimTask(CONTAINER_ID, tasks.get(0).getId(), USERNAME); 
-//        taskServicesClient.activateTask(CONTAINER_ID, tasks.get(0).getId(), USERNAME); 
+            logger.info("task: " + String.valueOf(taskSummary));
+        }
+
+//        taskServicesClient.claimTask(CONTAINER_ID, tasks.get(0).getId(), USERNAME);
+//        taskServicesClient.activateTask(CONTAINER_ID, tasks.get(0).getId(), USERNAME);
 //        taskServicesClient.startTask(CONTAINER_ID, tasks.get(0).getId(), USERNAME);
-        
         //taskServicesClient.completeTask(CONTAINER_ID, tasks.get(0).getId(), USERNAME, null);
         //taskServicesClient.completeAutoProgress(containerId, taskId, userId, params);
-        
 //        void completeTask(String containerId, Long taskId, String userId, Map<String, Object> params);
-
-
-        Map<String, Object> params = new HashMap<String,Object>();
+        Map<String, Object> params = new HashMap<String, Object>();
         params.put("taskOutputApplication", generateDummyApp());
 
         try {
             ObjectMapper mapper = new ObjectMapper();
             String val = mapper.writeValueAsString(params);
-            logger.info("json \n\n" +val);
-        }catch(Exception e){
+            logger.info("json \n\n" + val);
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         taskServicesClient.completeAutoProgress(CONTAINER_ID, tasks.get(0).getId(), USERNAME, params);
     }
-    
+
     @Test
     public void testMarshal() throws Exception {
-    		javax.xml.bind.JAXBContext jaxbContext = org.eclipse.persistence.jaxb.JAXBContext.newInstance(Application.class, HashMap.class);
-	    	Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-	    	jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-	    //jaxbMarshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
-	    	//jaxbMarshaller.setProperty(MarshallerProperties.JSON_INCLUDE_ROOT, false);
-	    	Map<String,Object> params = new HashMap<String,Object>();
-	    	params.put("taskOutputApplication", generateDummyApp());
-	    	jaxbMarshaller.marshal(params, System.out);
+        javax.xml.bind.JAXBContext jaxbContext = org.eclipse.persistence.jaxb.JAXBContext.newInstance(Application.class, HashMap.class);
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        //jaxbMarshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
+        //jaxbMarshaller.setProperty(MarshallerProperties.JSON_INCLUDE_ROOT, false);
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("taskOutputApplication", generateDummyApp());
+        jaxbMarshaller.marshal(params, System.out);
     }
-    
-    
+
     private Application generateDummyApp() {
-    		Application app = new Application();
-    		
-    		List<ValidationError> errors = new ArrayList<ValidationError>();
+        Application app = new Application();
+
+        List<ValidationError> errors = new ArrayList<ValidationError>();
 //    		errors.add(new ValidationError("cause1"));
 //    		errors.add(new ValidationError("cause2"));
-    		app.setValidationErrors(errors);
-    		
-    		app.setApplicant(new Applicant("bob", 123132123, 50000, 25));
-    		app.setAmortization(30);
-    		
-    		Property property = new Property("123 maple lane", 10000);
-    		app.setAppraisal(new Appraisal(property, new java.util.Date(), 150000));
-    		
-    		app.setApr(1.5d);
-    		app.setDownPayment(10000);
-    		app.setMortgageAmount(175500);
-            app.setProperty(property);
-    		return app;
+        app.setValidationErrors(errors);
+
+        app.setApplicant(new Applicant("bob", 123132123, 50000, 25));
+        app.setAmortization(30);
+
+        Property property = new Property("123 maple lane", 10000);
+        app.setAppraisal(new Appraisal(property, new java.util.Date(), 150000));
+
+        app.setApr(1.5d);
+        app.setDownPayment(10000);
+        app.setMortgageAmount(175500);
+        app.setProperty(property);
+        return app;
     }
 
 }
