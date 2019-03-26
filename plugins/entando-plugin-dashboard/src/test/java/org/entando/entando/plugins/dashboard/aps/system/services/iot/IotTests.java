c9f3fc0f637b38eb80335652d77911abd7c0be1f
Marco Cauli <marco.cauli@realt.it>
2019-03-26 09:21:17 +0100

    fix IotTests

--- plugins/entando-plugin-dashboard/src/test/java/org/entando/entando/plugins/dashboard/aps/system/services/iot/IotTests.java
+++ plugins/entando-plugin-dashboard/src/test/java/org/entando/entando/plugins/dashboard/aps/system/services/iot/IotTests.java
@@ -1,7 +1,5 @@
 package org.entando.entando.plugins.dashboard.aps.system.services.iot;
 
-import org.entando.entando.plugins.dashboard.aps.system.services.iot.services.TestConnectorService;
-import org.entando.entando.plugins.dashboard.aps.system.services.iot.services.TestMeasurementConfigService;
 import org.entando.entando.plugins.dashboard.aps.system.services.iot.utils.IotUtilsTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,9 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
 
-    TestConnectorService.class,
     IotUtilsTest.class,
-    TestMeasurementConfigService.class
 
 })
 public class IotTests { }
