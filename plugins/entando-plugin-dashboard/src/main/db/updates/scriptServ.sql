577c9f4f1f8e54a7d5207d6e7e39b75956f49dda
daniele.cherchi@realt.it <daniele.cherchi@realt.it>
2019-04-01 18:32:55 +0200

    aggiunta query

--- plugins/entando-plugin-dashboard/src/main/db/updates/scriptServ.sql
+++ plugins/entando-plugin-dashboard/src/main/db/updates/scriptServ.sql
@@ -5,5 +5,7 @@ ALTER TABLE dashboard_config_datasource ADD COLUMN metadata TEXT;
 ALTER TABLE dashboard_config_datasource ADD COLUMN name character varying(255);
   
 ALTER TABLE dashboard_config ADD COLUMN "type" character varying(100);
+
+ALTER TABLE dashboard_config_datasource ALTER COLUMN datasourceuri DROP NOT NULL;
   
     
