940a2e176e8f24931ec0317e8ed5c061d26e277b
Alessio Pintus <alessio.pintus@gmail.com>
2019-04-24 11:28:55 +0200

    EN-2319 Fields added for the datatable process list widget

--- plugins/entando-plugin-jpkiebpm/src/main/java/org/entando/entando/plugins/jpkiebpm/apsadmin/portal/specialwidget/BpmProcessDatatableWidgetAction.java
+++ plugins/entando-plugin-jpkiebpm/src/main/java/org/entando/entando/plugins/jpkiebpm/apsadmin/portal/specialwidget/BpmProcessDatatableWidgetAction.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Entando Inc..
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.entando.entando.plugins.jpkiebpm.apsadmin.portal.specialwidget;
 
 import com.agiletec.aps.system.exception.ApsSystemException;
@@ -16,10 +39,16 @@ public class BpmProcessDatatableWidgetAction extends BpmDatatableWidgetAction {
     protected void loadFieldIntoDatatableFromBpm(String containerId, String processId) throws ApsSystemException {
         KieBpmConfig config = getFormManager().getKieServerConfigurations().get(this.getKnowledgeSourcePath());
         List<KieProcess> processes = getFormManager().getProcessDefinitionsList(config);
+
+
+
         if (!processes.isEmpty()) {
 
             //Horrible hack. To be replaced by an actual call.
             List<String> fields = new ArrayList<>();
+
+            logger.info("***************************** \n"+processes.get(0).toString()+"\n*****************************");
+
             StringTokenizer tokenizer = new StringTokenizer(processes.get(0).toString(), ",");
             Byte position = 1;
             while (tokenizer.hasMoreTokens()) {
@@ -32,14 +61,6 @@ public class BpmProcessDatatableWidgetAction extends BpmDatatableWidgetAction {
         }
 
         Map<String, String> columns = new HashMap<>();
-
-        columns.put("Status Progress", "statusProgress");
-        columns.put("Customer Name", "customerName");
-        columns.put("partyName", "partyName");
-        columns.put("status", "status");
-        columns.put("Company", "company");
-        columns.put("Case Due In", "dueDate");
-
         this.loadDataIntoFieldDatatable(columns);
     }
 
