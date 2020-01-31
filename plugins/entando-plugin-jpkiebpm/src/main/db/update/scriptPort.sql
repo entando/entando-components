a0de3159999eaed75af7248242139cb1e627fb2f
Werbth Rocha <werbth@gmail.com>
2020-01-31 01:23:03 -0300

    ENG-103: Update Widget API

--- plugins/entando-plugin-jpkiebpm/src/main/db/update/scriptPort.sql
+++ plugins/entando-plugin-jpkiebpm/src/main/db/update/scriptPort.sql
@@ -1,7 +1,7 @@
 DELETE FROM widgetcatalog WHERE code ='bpm-datatable';
 DELETE FROM widgetcatalog WHERE code ='bpm-datatable-task-list';
 
-INSERT INTO widgetcatalog VALUES ('bpm-datatable-task-list', '<?xml version="1.0" encoding="UTF-8"?>
+INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('bpm-datatable-task-list', '<?xml version="1.0" encoding="UTF-8"?>
 <properties>
 <property key="en">BPM-Datatable Task List</property>
 <property key="it">BPM-Datatable Task List</property>
@@ -10,7 +10,7 @@ INSERT INTO widgetcatalog VALUES ('bpm-datatable-task-list', '<?xml version="1.0
 	<action name="jpkiebpmBpmTaskListDatatableWidgetViewerConfig"/>
 </config>', 'jpkiebpm', NULL, NULL, 1, NULL);
 
-INSERT INTO widgetcatalog VALUES ('bpm-datatable-process-list', '<?xml version="1.0" encoding="UTF-8"?>
+INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('bpm-datatable-process-list', '<?xml version="1.0" encoding="UTF-8"?>
 <properties>
 <property key="en">BPM-Datatable Process list</property>
 <property key="it">BPM-Datatable Lista processi</property>
