39872f96e2a0957d589396ccce7fa1ae52c24eab
daniele.cherchi@realt.it <daniele.cherchi@realt.it>
2020-02-28 11:40:24 +0100

    fix deplublicazione per contenuto sostitutivo posto sul contenttype

--- plugins/entando-plugin-jpcontentscheduler/src/main/java/org/entando/entando/plugins/jpcontentscheduler/aps/system/services/content/ContentJobs.java
+++ plugins/entando-plugin-jpcontentscheduler/src/main/java/org/entando/entando/plugins/jpcontentscheduler/aps/system/services/content/ContentJobs.java
@@ -268,21 +268,15 @@ public class ContentJobs extends QuartzJobBean implements ApplicationContextAwar
 								// nelle pagine.
 								List<IPage> listaPagine = references.get("CmsPageManagerWrapperUtilizers");
 								if (listaPagine != null && listaPagine.size() > 0) {
-									String fieldname = c.getFieldNameContentReplace();
+									String contentIdOfContentToPublish = c.getFieldNameContentReplace();
 									String contenutoSostitutivo = null;
-									if (fieldname != null && !fieldname.isEmpty()) {
-										if (contentToSuspend.getAttributeMap().get(fieldname) != null) {
-											contenutoSostitutivo = contentToSuspend.getAttributeMap().get(fieldname)
-													.getValue().toString();
-										}
+									if (contentIdOfContentToPublish != null && !contentIdOfContentToPublish.isEmpty()) {
+                                                                                contenutoSostitutivo = contentIdOfContentToPublish;
 									}
-									String fieldnameModel = c.getFieldNameModelContentReplace();
+									String contentModelIdOfContentToPublish = c.getFieldNameModelContentReplace();
 									String modelContenutoSostitutivo = null;
-									if (fieldnameModel != null && !fieldnameModel.isEmpty()) {
-										if (contentToSuspend.getAttributeMap().get(fieldnameModel) != null) {
-											modelContenutoSostitutivo = contentToSuspend.getAttributeMap()
-													.get(fieldnameModel).getValue().toString();
-										}
+									if (contentModelIdOfContentToPublish != null && !contentModelIdOfContentToPublish.isEmpty()) {
+                                                                            modelContenutoSostitutivo = contentModelIdOfContentToPublish;
 									}
 									// Contenuto sostitutivo impostato nel caso
 									// in cui non sia presente il campo per il
@@ -373,6 +367,7 @@ public class ContentJobs extends QuartzJobBean implements ApplicationContextAwar
 											} // fine ciclo dei widget
 											if (framePos != -1 && widget != null) {
 												this.getPageManager().joinWidget(page.getCode(), widget, framePos);
+                                                                                                this.getPageManager().setPageOnline(page.getCode());
 												operation = "replaceContent";
 											}
 										}
@@ -626,6 +621,7 @@ public class ContentJobs extends QuartzJobBean implements ApplicationContextAwar
 											} // fine ciclo dei widget
 											if (framePos != -1 && widget != null) {
 												this.getPageManager().joinWidget(page.getCode(), widget, framePos);
+                                                                                                this.getPageManager().setPageOnline(page.getCode());
 												operation = "replaceContent";
 											}
 										}
