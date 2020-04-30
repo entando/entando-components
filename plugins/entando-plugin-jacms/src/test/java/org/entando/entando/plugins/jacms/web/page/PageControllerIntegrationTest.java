1e239d770552d504a7434a6a18cae7a6f47e5c90
Filipe Leandro <ffleandro>
2020-04-30 08:39:25 -0300

    ENG-386 rollbacking unecessary changes

--- plugins/entando-plugin-jacms/src/test/java/org/entando/entando/plugins/jacms/web/page/PageControllerIntegrationTest.java
+++ plugins/entando-plugin-jacms/src/test/java/org/entando/entando/plugins/jacms/web/page/PageControllerIntegrationTest.java
@@ -13,14 +13,30 @@
  */
 package org.entando.entando.plugins.jacms.web.page;
 
+import com.agiletec.aps.system.services.page.PagesStatus;
 import com.agiletec.aps.system.services.user.UserDetails;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.http.entity.ContentType;
+import org.entando.entando.aps.system.services.page.IPageService;
+import org.entando.entando.aps.system.services.page.PageServiceIntegrationTest;
+import org.entando.entando.aps.system.services.page.model.PageDto;
 import org.entando.entando.web.AbstractControllerIntegrationTest;
+import org.entando.entando.web.page.model.PageRequest;
+import org.entando.entando.web.page.model.WidgetConfigurationRequest;
 import org.entando.entando.web.utils.OAuth2TestUtils;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,6 +45,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author paddeo
  */
 public class PageControllerIntegrationTest extends AbstractControllerIntegrationTest {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    @Autowired
+    private IPageService pageService;
 
     @Test
     public void testPageGet() throws Exception {
@@ -36,7 +56,8 @@ public class PageControllerIntegrationTest extends AbstractControllerIntegration
         String accessToken = mockOAuthInterceptor(user);
         ResultActions result = mockMvc
                 .perform(get("/pages/{code}", "pagina_11")
-                        .header("Authorization", "Bearer " + accessToken));
+                        .header("Authorization", "Bearer " + accessToken))
+                .andDo(print());
 
         result.andExpect(status().isOk());
         result.andExpect(jsonPath("$.payload.code", is("pagina_11")));
@@ -53,6 +74,56 @@ public class PageControllerIntegrationTest extends AbstractControllerIntegration
 
         result.andExpect(status().isOk());
         result.andExpect(jsonPath("$.metaData.totalItems", is(2)));
+    }
+
+    @Test
+    public void testPageAddUpdateDelete() throws Exception {
+        String pageCode = "page_update_test";
+        String widgetCode = "content_viewer";
+
+        PageDto pageToClone = pageService.getPage("pagina_11", IPageService.STATUS_DRAFT);
+
+        PageRequest pageRequest = PageServiceIntegrationTest.createRequestFromDto(pageToClone);
+        pageRequest.setCode(pageCode);
+        pageRequest.setStatus(IPageService.STATUS_DRAFT);
+
+        WidgetConfigurationRequest widgetRequest = new WidgetConfigurationRequest();
+        widgetRequest.setCode(widgetCode);
+        widgetRequest.setConfig(new HashMap<>());
+
+        UserDetails user = new OAuth2TestUtils.UserBuilder("jack_bauer", "0x24").grantedToRoleAdmin().build();
+        String accessToken = mockOAuthInterceptor(user);
+
+        try {
+            mockMvc.perform(post("/pages", pageCode)
+                    .header("Authorization", "Bearer " + accessToken)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(MAPPER.writeValueAsString(pageRequest)))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.payload.code", is(pageCode)))
+                    .andExpect(jsonPath("$.payload.numWidget", is(0)));
+
+            mockMvc.perform(put("/pages/{code}/widgets/{frame}", pageCode, 0)
+                    .header("Authorization", "Bearer " + accessToken)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(MAPPER.writeValueAsString(widgetRequest)))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+
+            mockMvc.perform(put("/pages/{code}", pageCode)
+                    .header("Authorization", "Bearer " + accessToken)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(MAPPER.writeValueAsString(pageRequest)))
+                    .andDo(print()).andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.payload.code", is(pageCode)))
+                    .andExpect(jsonPath("$.payload.numWidget", is(1)));
+        } finally {
+            mockMvc.perform(delete("/pages/{code}", pageCode)
+                    .header("Authorization", "Bearer " + accessToken))
+                    .andExpect(status().isOk());
+        }
     }
 
 }
