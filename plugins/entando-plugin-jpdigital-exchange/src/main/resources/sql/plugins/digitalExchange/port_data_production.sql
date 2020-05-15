371629e353b11336bb0b5e32cddab9908543a1df
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2020-05-15 13:28:08 +0200

    ENG-549: Deleted DE component (#642)
    # Conflicts:
    #	plugins/entando-plugin-jpdigital-exchange/pom.xml

--- plugins/entando-plugin-jpdigital-exchange/src/main/resources/sql/plugins/digitalExchange/port_data_production.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO sysconfig (version, item, descr, config) VALUES ('production', 'digitalExchanges', 'Available Digital Exchange instances', '<?xml version="1.0" encoding="UTF-8"?>
-<digitalExchanges></digitalExchanges>');
-INSERT INTO sysconfig (version, item, descr, config) VALUES ('production', 'digitalExchangeCategories', 'Supported Digital Exchange categories', 'widget,pageModel,component,fragment,contentModel,contentType');
